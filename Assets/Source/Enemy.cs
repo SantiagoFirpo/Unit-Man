7d2fab1d98bb8b55e8e8a2c83534a18849042d32
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-05-19 17:13:06 -0300

    Start Enemy Random Movement

--- Assets/Source/Enemy.cs
+++ Assets/Source/Enemy.cs
@@ -1,4 +1,5 @@
 using System;
+using UnitMan.Source.Utilities.TimeTracking;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,7 +7,29 @@ namespace UnitMan.Source
 {
     public class Enemy : Actor
     {
-       
+       private readonly Timer _directionTimer = new Timer(targetOneShot: false);
+       private const float MOVE_SPEED = 5f;
+
+       protected override void Awake()
+       {
+           base.Awake();
+           _directionTimer.OnEnd += ChangeDirection;
+       }
+
+       private void ChangeDirection()
+       {
+           base.rigidBody.velocity = MOVE_SPEED *
+                                     new Vector2(Mathf.Round(UnityEngine.Random.Range(0f,
+                                             1f)),
+                                         Mathf.Round(UnityEngine.Random.Range(0f,
+                                             1f)));
+       }
+
+       private void Start()
+       {
+           _directionTimer.Begin();
+           
+       }
     }
 }
 
