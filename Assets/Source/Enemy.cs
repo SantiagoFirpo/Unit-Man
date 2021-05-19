acf3fdc82f767c74a0ae759fb60b8ca042e54eb2
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-05-19 17:32:30 -0300

    Random Enemy Movement

--- Assets/Source/Enemy.cs
+++ Assets/Source/Enemy.cs
@@ -3,32 +3,45 @@ using UnitMan.Source.Utilities.TimeTracking;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace UnitMan.Source
-{
-    public class Enemy : Actor
-    {
+namespace UnitMan.Source {
+    public class Enemy : Actor {
        private readonly Timer _directionTimer = new Timer(targetOneShot: false);
-       private const float MOVE_SPEED = 5f;
+       private const int MOVE_SPEED = 5;
 
-       protected override void Awake()
-       {
+       protected override void Awake() {
            base.Awake();
            _directionTimer.OnEnd += ChangeDirection;
        }
 
        private void ChangeDirection()
        {
-           base.rigidBody.velocity = MOVE_SPEED *
-                                     new Vector2(Mathf.Round(UnityEngine.Random.Range(0f,
-                                             1f)),
-                                         Mathf.Round(UnityEngine.Random.Range(0f,
-                                             1f)));
+           Debug.Log(_directionTimer.paused);
+           base.rigidBody.velocity = GetRandomRoundedVector2();
+       }
+       
+       private void Start() {
+           _directionTimer.Begin();
        }
 
-       private void Start()
+       private void Update() {
+           if (base.rigidBody.velocity == Vector2.zero) {
+               base.rigidBody.velocity = Vector2Int.Vector2(GetRandomRoundedVector2());
+           }
+       }
+       
+       private Vector2Int GetRandomRoundedVector2()
        {
-           _directionTimer.Begin();
-           
+           return MOVE_SPEED * new Vector2Int(
+               GetRandomInt(-1, 1),
+               GetRandomInt(-1, 1));
+       }
+
+       private static int GetRandomInt(int smallestInclusive, int biggestInclusive)
+       {
+           return Mathf.RoundToInt(
+               UnityEngine.Random.Range(
+                   smallestInclusive,
+                   biggestInclusive));
        }
     }
 }
