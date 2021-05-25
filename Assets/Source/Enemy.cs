033e8ad71f591043f9e4d4f127e0efc241e36d6b
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-05-25 17:25:07 -0300

    Add A* Agent to Enemy base class

--- Assets/Source/Enemy.cs
+++ Assets/Source/Enemy.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using UnitMan.Source.Utilities.AI;
 using UnitMan.Source.Utilities.TimeTracking;
 using UnityEngine;
 
@@ -5,40 +8,39 @@ namespace UnitMan.Source {
     public class Enemy : Actor {
        private readonly Timer _directionTimer = new Timer(targetOneShot: false);
        private const int MOVE_SPEED = 5;
+       private Agent _agent;
+       private Queue<PathNode> _nodeQueue = new Queue<PathNode>();
+
+       [SerializeField] private Transform _playerTransform;
 
        protected override void Awake() {
            base.Awake();
-           _directionTimer.OnEnd += ChangeDirection;
+           _agent = GetComponent<Agent>();
        }
 
-       private void ChangeDirection()
-       {
-           Debug.Log(_directionTimer.paused);
-           motion = GetRandomCardinal();
-           while (motion == Vector2.zero)
-           {
-               motion = GetRandomCardinal();
-           }
-       }
-       
        private void Start() {
            _directionTimer.Begin();
+           _nodeQueue = ShortestPathToPlayer();
        }
 
-       private void FixedUpdate()
-       {
-           rigidBody.velocity = motion;
-           if (rigidBody.velocity == Vector2.zero) {
-               rigidBody.velocity =  (Vector2) (GetRandomCardinal());
+       private Queue<PathNode> ShortestPathToPlayer() {
+           return _agent.ShortestPathBetween(Vector2Int.RoundToInt(_transform.position), Vector2Int.RoundToInt(_playerTransform.position));
+       }
+
+       private void FixedUpdate() {
+           Vector2 nextPosition = _nodeQueue.Peek().position;
+           motion = nextPosition;
+           _rigidBody.velocity = motion;
+           if ((Vector2) _transform.position == nextPosition) {
+               _nodeQueue.Dequeue();
            }
        }
+       // private static Vector2Int GetRandomCardinal()
+       // {
+       //     return MOVE_SPEED * new Vector2Int(UnityEngine.Random.Range(-1, 2), UnityEngine.Random.Range(-1, 2));
+       // }
        
-       private static Vector2Int GetRandomCardinal()
-       {
-           return MOVE_SPEED * new Vector2Int(UnityEngine.Random.Range(-1, 2), UnityEngine.Random.Range(-1, 2));
-       }
+       
        
     }
 }
-
-
