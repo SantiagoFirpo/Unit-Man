21e8d5bc5944d6fa13810f14200864c73b523786
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-05-26 17:29:26 -0300

    Disable pathfinding until bugs are fixed

--- Assets/Source/Enemy.cs
+++ Assets/Source/Enemy.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using UnitMan.Source.Utilities;
 using UnitMan.Source.Utilities.AI;
 using UnitMan.Source.Utilities.TimeTracking;
 using UnityEngine;
@@ -24,17 +25,17 @@ namespace UnitMan.Source {
        }
 
        private Queue<PathNode> ShortestPathToPlayer() {
-           return _agent.ShortestPathBetween(Vector2Int.RoundToInt(_transform.position), Vector2Int.RoundToInt(_playerTransform.position));
+           return Astar.ShortestPathBetween(Vector2Int.RoundToInt(_transform.position), Vector2Int.RoundToInt(_playerTransform.position));
        }
 
-       private void FixedUpdate() {
-           Vector2 nextPosition = _nodeQueue.Peek().position;
-           motion = nextPosition;
-           _rigidBody.velocity = motion;
-           if ((Vector2) _transform.position == nextPosition) {
-               _nodeQueue.Dequeue();
-           }
-       }
+       // private void FixedUpdate() {
+       //     Vector2 nextPosition = _nodeQueue.Peek().position;
+       //     motion = nextPosition;
+       //     _rigidBody.velocity = motion;
+       //     if ((Vector2) _transform.position == nextPosition) {
+       //         _nodeQueue.Dequeue();
+       //     }
+       // }
        // private static Vector2Int GetRandomCardinal()
        // {
        //     return MOVE_SPEED * new Vector2Int(UnityEngine.Random.Range(-1, 2), UnityEngine.Random.Range(-1, 2));
