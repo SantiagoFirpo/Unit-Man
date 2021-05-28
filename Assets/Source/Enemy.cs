9d6940df783c1fce53a96d480435137213146983
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-05-28 15:03:17 -0300

    Restore enemy movement

--- Assets/Source/Enemy.cs
+++ Assets/Source/Enemy.cs
@@ -4,44 +4,52 @@ using UnitMan.Source.Utilities;
 using UnitMan.Source.Utilities.AI;
 using UnitMan.Source.Utilities.TimeTracking;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace UnitMan.Source {
     public class Enemy : Actor {
-       private readonly Timer _directionTimer = new Timer(targetOneShot: false);
-       private const int MOVE_SPEED = 5;
+       private readonly Timer _directionTimer = new Timer(PATHFIND_INTERVAL_SECONDS, 0f, true, false);
+       private const float MOVE_SPEED = 0.05f;
+       private const float FIXED_MOVE_SPEED = MOVE_SPEED / 50f;
        private Agent _agent;
        private Queue<PathNode> _nodeQueue = new Queue<PathNode>();
 
-       [SerializeField] private Transform _playerTransform;
+       [SerializeField]
+       private Transform playerTransform;
+
+       private const float PATHFIND_INTERVAL_SECONDS = 4f;
 
        protected override void Awake() {
            base.Awake();
            _agent = GetComponent<Agent>();
+           UpdatePath();
+           _directionTimer.OnEnd += UpdatePath;
        }
 
        private void Start() {
            _directionTimer.Begin();
-           _nodeQueue = ShortestPathToPlayer();
        }
 
        private Queue<PathNode> ShortestPathToPlayer() {
-           return Astar.ShortestPathBetween(Vector2Int.RoundToInt(_transform.position), Vector2Int.RoundToInt(_playerTransform.position));
+           return AStar.ShortestPathBetween(Vector2Int.RoundToInt(_transform.position), Vector2Int.RoundToInt(playerTransform.position));
        }
 
-       // private void FixedUpdate() {
-       //     Vector2 nextPosition = _nodeQueue.Peek().position;
-       //     motion = nextPosition;
-       //     _rigidBody.velocity = motion;
-       //     if ((Vector2) _transform.position == nextPosition) {
-       //         _nodeQueue.Dequeue();
-       //     }
-       // }
-       // private static Vector2Int GetRandomCardinal()
-       // {
-       //     return MOVE_SPEED * new Vector2Int(UnityEngine.Random.Range(-1, 2), UnityEngine.Random.Range(-1, 2));
-       // }
-       
-       
-       
+       private void FixedUpdate() {
+           if (_nodeQueue.Count == 0) return;
+           MoveThroughPath();
+       }
+
+       private void MoveThroughPath() {
+           Vector2 nextPosition = _nodeQueue.Peek().position;
+           _transform.position = Vector2.MoveTowards(_transform.position, nextPosition, MOVE_SPEED);
+           _rigidBody.velocity = motion;
+           if ((Vector2) _transform.position == nextPosition) {
+               _nodeQueue.Dequeue();
+           }
+       }
+
+       private void UpdatePath() {
+           _nodeQueue = ShortestPathToPlayer();
+       }
     }
 }
