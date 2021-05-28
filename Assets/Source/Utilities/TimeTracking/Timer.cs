c4353fa92e3e21a04b618c17a5e01912589e819a
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-05-28 15:00:10 -0300

    Refactor to abide by conventions

--- Assets/Source/Utilities/TimeTracking/Timer.cs
+++ Assets/Source/Utilities/TimeTracking/Timer.cs
@@ -6,51 +6,52 @@ namespace UnitMan.Source.Utilities.TimeTracking {
     //Responsibility: Abstract model for finite timers inside other classes
     public delegate void TimerChange();
     public event TimerChange OnEnd;
-    float waitTime;
-    float delay;
-    float currentTime = 0f;
-    bool autoStart;
-    bool oneShot;
+    private readonly float _waitTime;
+    private readonly float _delay;
+    private float _currentTime = 0f;
+    private readonly bool _autoStart;
+    private readonly bool _oneShot;
 
     public bool paused;
-    public void Update(float deltaTime) {
-        if (!paused && currentTime < waitTime) {
-            currentTime += Time.deltaTime;
+
+    private void Update(float deltaTime) {
+        if (!paused && _currentTime < _waitTime) {
+            _currentTime += Time.deltaTime;
         }
-        else if (!paused && currentTime >= waitTime){
+        else if (!paused && _currentTime >= _waitTime){
             OnEnd?.Invoke();
-            if (oneShot) {
+            if (_oneShot) {
                 //Timer ended but is oneShot
                 paused = true;
-                currentTime = Mathf.Round(currentTime);
+                _currentTime = Mathf.Round(_currentTime);
                 
             }
             else {
                 //Timer ended and reset
-                currentTime = 0f;
+                _currentTime = 0f;
                 paused = false;
             }
         }
             
     }
     public void Begin() {
-        currentTime = 0f;
+        _currentTime = 0f;
         paused = false;
     }
     public void Stop() {
-        currentTime = 0f;
+        _currentTime = 0f;
         paused = true;
     }
     public void Setup() {
-        currentTime = 0f;
-        paused = !autoStart;
+        _currentTime = 0f;
+        paused = !_autoStart;
     }
 
     public Timer(float targetWaitTime = 1, float targetDelay = 0, bool targetAutoStart = false, bool targetOneShot = true) {
-        waitTime = targetWaitTime;
-        delay = targetDelay;
-        autoStart = targetAutoStart;
-        oneShot = targetOneShot;
+        _waitTime = targetWaitTime;
+        _delay = targetDelay;
+        _autoStart = targetAutoStart;
+        _oneShot = targetOneShot;
         TimerManager.OnFrameUpdate += Update;
         TimerManager.Initialized += Setup;
     }
