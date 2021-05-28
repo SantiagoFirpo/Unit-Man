58fc9b41292066d14da20cfef602289409434328
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-05-28 15:02:48 -0300

    Add second raycast to each player direction and refactor CheckPossibleTurns method

--- Assets/Source/PlayerController.cs
+++ Assets/Source/PlayerController.cs
@@ -5,6 +5,7 @@ using UnitMan.Source.Utilities;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
+using static UnityEngine.Vector2Int;
 
 namespace UnitMan.Source
 {
@@ -15,7 +16,8 @@ namespace UnitMan.Source
         [SerializeField] private int _previousState;
 
         private const float MOVE_SPEED = 3f;
-        private const float WALL_CHECK_DISTANCE = 0.6f;
+        private const float WALL_CHECK_DISTANCE = 0.8f;
+        const float ALMOST_ONE = 0.9f;
 
         private StateMachine _stateMachine;
         private ITransitionProvider _transitionProvider;
@@ -28,10 +30,26 @@ namespace UnitMan.Source
 
         private Vector2Int _currentDirection;
         private LayerMask _wallLayer;
+        
+        private readonly Vector2 _upLeft = new Vector2(-0.5f, 0.5f);
+        private readonly Vector2 _upRight = new Vector2(0.5f, 0.5f);
 
+        private readonly Vector2 _downLeft = new Vector2(-0.5f, -0.5f);
+        private readonly Vector2 _downRight = new Vector2(0.5f, -0.5f);
 
-        [FormerlySerializedAs("_wallChecks")] [SerializeField]
-        private Vector2Int[] _possibleTurns;
+        private Vector2 _almostUpLeft;
+        private Vector2 _almostUpRight;
+        private Vector2 _almostDownLeft;
+        private Vector2 _almostDownRight;
+        
+        Vector2 playerPosition;
+        
+        private readonly bool[] _possibleTurns = {false, false, false, false};
+
+        private enum Turns
+        {
+            Up = 0, Down = 1, Left = 2, Right = 3
+        }
 
 
         public enum PlayerState
@@ -54,47 +72,74 @@ namespace UnitMan.Source
             _playerInput = GetComponent<PlayerInput>();
             _playerInput.onActionTriggered += OnInputChanged;
             _wallLayer = LayerMask.GetMask("Wall");
+
+            _almostUpLeft = _upLeft * ALMOST_ONE;
+            _almostUpRight = _upRight * ALMOST_ONE;
+            _almostDownLeft = _downLeft * ALMOST_ONE;
+            _almostDownRight = _downRight * ALMOST_ONE;
         }
 
         private void FixedUpdate() {
-            _possibleTurns = CheckPossibleTurns();
-            if (IsCardinalDirection(_inputVector) && _possibleTurns.Contains(_inputVector))
+            playerPosition = _transform.position;
+            if (!IsCardinalDirection(_inputVector)) return;
+            CheckPossibleTurns();
+            int index = GetDirectionIndex(_inputVector);
+
+            if (_possibleTurns[index]) {
                 _currentDirection = _inputVector;
+            }
+                
 
             motion = (Vector2) _currentDirection * MOVE_SPEED;
 
-            if (CanTurn()) _rigidBody.velocity = motion;
+            // if (_rigidBody.velocity == Vector2.zero) {
+            //     _transform.position = Vector3Int.RoundToInt(_transform.position);
+            // }
+
+            _rigidBody.velocity = motion;
         }
 
-        private void PollBufferedInput() {
-            bool isInBufferWindow = Time.realtimeSinceStartup - inputContext.startTime <= BUFFER_WINDOW_SECONDS;
-            if (isInBufferWindow && CanTurn()) _rigidBody.velocity = motion;
+        private static int GetDirectionIndex(Vector2Int vector) {
+            int index = -1;
+            if (vector == up) {
+                index = 0;
+            }
+            else if (vector == down) {
+                index = 1;
+            }
+            else if (vector == left) {
+                index = 2;
+            }
+            else if (vector == right) {
+                index = 3;
+            }
+
+            return index;
         }
 
         private bool CanTurn() {
-            return ((IList) _possibleTurns).Contains(_currentDirection);
+            return _possibleTurns[GetDirectionIndex(_inputVector)];
         }
 
-        private void OnCollisionEnter2D(Collision2D other) {
-            if (other.gameObject.layer == _wallLayer)
-                if (_possibleTurns.Length == 2)
-                    _currentDirection = _possibleTurns[0];
-        }
+        private void CheckPossibleTurns() {
+            playerPosition = _transform.position;
+            
+            RaycastHit2D upHitOne = Physics2D.Raycast(playerPosition + _almostUpLeft, Vector2.up, WALL_CHECK_DISTANCE, _wallLayer);
+            RaycastHit2D upHitTwo = Physics2D.Raycast(playerPosition + _almostUpRight, Vector2.up, WALL_CHECK_DISTANCE, _wallLayer);
 
-        private Vector2Int[] CheckPossibleTurns() {
-            Vector2 playerPosition = _transform.position;
-            RaycastHit2D upHit = Physics2D.Raycast(playerPosition, Vector2.up, WALL_CHECK_DISTANCE, _wallLayer);
-            RaycastHit2D downHit = Physics2D.Raycast(playerPosition, Vector2.down, WALL_CHECK_DISTANCE, _wallLayer);
-            RaycastHit2D leftHit = Physics2D.Raycast(playerPosition, Vector2.left, WALL_CHECK_DISTANCE, _wallLayer);
-            RaycastHit2D rightHit = Physics2D.Raycast(playerPosition, Vector2.right, WALL_CHECK_DISTANCE, _wallLayer);
+            RaycastHit2D downHitOne = Physics2D.Raycast(playerPosition + _almostDownLeft, Vector2.down, WALL_CHECK_DISTANCE, _wallLayer);
+            RaycastHit2D downHitTwo = Physics2D.Raycast(playerPosition + _almostDownRight, Vector2.down, WALL_CHECK_DISTANCE, _wallLayer);
 
-            List<Vector2Int> results = new List<Vector2Int>();
-            if (!upHit) results.Add(Vector2Int.up);
-            if (!downHit) results.Add(Vector2Int.down);
-            if (!leftHit) results.Add(Vector2Int.left);
-            if (!rightHit) results.Add(Vector2Int.right);
-
-            return results.ToArray();
+            RaycastHit2D leftHitOne = Physics2D.Raycast(playerPosition + _almostDownLeft, Vector2.left, WALL_CHECK_DISTANCE, _wallLayer);
+            RaycastHit2D leftHitTwo = Physics2D.Raycast(playerPosition + _almostUpLeft, Vector2.left, WALL_CHECK_DISTANCE, _wallLayer);
+            
+            RaycastHit2D rightHitOne = Physics2D.Raycast(playerPosition + _almostDownRight, Vector2.right, WALL_CHECK_DISTANCE, _wallLayer);
+            RaycastHit2D rightHitTwo = Physics2D.Raycast(playerPosition + _almostUpRight, Vector2.right, WALL_CHECK_DISTANCE, _wallLayer);
+            
+            _possibleTurns[0] = !(upHitOne.collider || upHitTwo.collider);
+            _possibleTurns[1] = !(downHitOne.collider || downHitTwo.collider);
+            _possibleTurns[2] = !(leftHitOne.collider || leftHitTwo.collider);
+            _possibleTurns[3] = !(rightHitOne.collider || rightHitTwo.collider);
         }
 
 
@@ -104,16 +149,25 @@ namespace UnitMan.Source
 
         private void OnInputChanged(InputAction.CallbackContext context) {
             inputContext = context;
-            switch (inputContext.action.name) {
-                case "Move": {
-                    _inputVector = Vector2Int.RoundToInt(inputContext.ReadValue<Vector2>());
-                    break;
-                }
-            }
+            _inputVector = inputContext.action.name switch {
+                "Move" => RoundToInt(inputContext.ReadValue<Vector2>()),
+                _ => _inputVector
+            };
         }
 
         private void OnDrawGizmos() {
-            Debug.DrawRay(transform.position, Vector3.up * WALL_CHECK_DISTANCE, Color.green);
+            Vector3 position = transform.position;
+            Debug.DrawRay(position + (Vector3) _upLeft * ALMOST_ONE, Vector2.up * WALL_CHECK_DISTANCE, Color.green);
+            Debug.DrawRay(position + (Vector3) _upRight * ALMOST_ONE, Vector2.up * WALL_CHECK_DISTANCE, Color.green);
+            
+            Debug.DrawRay(position + (Vector3) _downLeft * ALMOST_ONE, Vector2.down * WALL_CHECK_DISTANCE, Color.green);
+            Debug.DrawRay(position + (Vector3) _downRight * ALMOST_ONE, Vector2.down * WALL_CHECK_DISTANCE, Color.green);
+            
+            Debug.DrawRay(position + (Vector3) _downLeft * ALMOST_ONE, Vector2.left * WALL_CHECK_DISTANCE, Color.green);
+            Debug.DrawRay(position + (Vector3) _upLeft * ALMOST_ONE, Vector2.left * WALL_CHECK_DISTANCE, Color.green);
+            
+            Debug.DrawRay(position + (Vector3) _downRight * ALMOST_ONE, Vector2.right * WALL_CHECK_DISTANCE, Color.green);
+            Debug.DrawRay(position + (Vector3) _upRight * ALMOST_ONE, Vector2.right * WALL_CHECK_DISTANCE, Color.green);
         }
     }
 }
