e832bcea387f937e59280cf6adfbff844d075170
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-05-28 14:59:47 -0300

    Remove unused members

--- Assets/Source/Utilities/TimeTracking/TimerManager.cs
+++ Assets/Source/Utilities/TimeTracking/TimerManager.cs
@@ -1,14 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace UnitMan.Source.Utilities.TimeTracking {
     public class TimerManager : MonoBehaviour
     {
-        public delegate void FrameUpdate(float deltaTime);
-        public static event FrameUpdate OnFrameUpdate;
-        public delegate void Init();
-        public static event System.Action Initialized;
-        void Update() => OnFrameUpdate?.Invoke(Time.deltaTime);
+        public static event Action<float> OnFrameUpdate;
+        public static event Action Initialized;
+        private void Update() => OnFrameUpdate?.Invoke(Time.deltaTime);
 
-        void Start() => Initialized?.Invoke();
+        private void Start() => Initialized?.Invoke();
     }
 }
