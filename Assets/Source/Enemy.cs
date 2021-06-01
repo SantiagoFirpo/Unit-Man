cc21af05008ea4cc3311a07fbe737362b687e0ca
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-06-01 15:31:58 -0300

    Use Rigidbody2D.velocity instead of Vector2.MoveTowards

--- Assets/Source/Enemy.cs
+++ Assets/Source/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnitMan.Source.Utilities.Pathfinding;
 using UnitMan.Source.Utilities.TimeTracking;
@@ -5,8 +6,8 @@ using UnityEngine;
 
 namespace UnitMan.Source {
     public class Enemy : Actor {
-       private readonly Timer _directionTimer = new Timer(PATHFIND_INTERVAL_SECONDS, 0f, true, false);
-       private const float MOVE_SPEED = 0.05f;
+       private readonly Timer _directionTimer = new Timer(PATHFINDING_INTERVAL_SECONDS, 0f, true, false);
+       private const float MOVE_SPEED = 1f;
        private const float FIXED_MOVE_SPEED = MOVE_SPEED / 50f;
        private Agent _agent;
        private Queue<PathNode> _nodeQueue = new Queue<PathNode>();
@@ -14,10 +15,14 @@ namespace UnitMan.Source {
        [SerializeField]
        private Transform playerTransform;
 
-       private const float PATHFIND_INTERVAL_SECONDS = 4f;
+       private Vector2Int _gridPosition;
+       private Vector2Int _direction;
+
+       private const float PATHFINDING_INTERVAL_SECONDS = 4f;
 
        protected override void Awake() {
            base.Awake();
+           startPosition = new Vector3(1f, 0f, 0f);
            _agent = GetComponent<Agent>();
            UpdatePath();
            _directionTimer.OnEnd += UpdatePath;
@@ -31,22 +36,38 @@ namespace UnitMan.Source {
            return AStar.ShortestPathBetween(Vector2Int.RoundToInt(_transform.position), Vector2Int.RoundToInt(playerTransform.position));
        }
 
-       private void FixedUpdate() {
+       protected override void FixedUpdate() {
+           base.FixedUpdate();
+           UpdateGridPosition();
            if (_nodeQueue.Count == 0) return;
            MoveThroughPath();
        }
 
+       
+
+       private void UpdateGridPosition() {
+           _gridPosition = Vector2Int.RoundToInt(_transform.position);
+       }
+
        private void MoveThroughPath() {
-           Vector2 nextPosition = _nodeQueue.Peek().position;
-           _transform.position = Vector2.MoveTowards(_transform.position, nextPosition, MOVE_SPEED);
+           Vector2Int nextPosition = _nodeQueue.Peek().position;
+           Vector2Int actualDirection = nextPosition - _gridPosition;
+           _direction = actualDirection == Vector2Int.zero ? _direction : actualDirection;
+           motion = _direction * (int) MOVE_SPEED;
+           // _transform.position = Vector2.MoveTowards(_transform.position, _gridPosition + _direction, FIXED_MOVE_SPEED);
            _rigidBody.velocity = motion;
-           if ((Vector2) _transform.position == nextPosition) {
+           if (VectorApproximately(_transform.position, nextPosition, 0.05f)) {
                _nodeQueue.Dequeue();
            }
+       }
+
+       private static bool VectorApproximately(Vector3 v1, Vector2Int v2, float maxDelta) {
+           return (Mathf.Abs(v1.x - v2.x) <= maxDelta && Mathf.Abs(v1.y - v2.y) <= maxDelta);
        }
 
        private void UpdatePath() {
            _nodeQueue = ShortestPathToPlayer();
        }
+       
     }
 }
