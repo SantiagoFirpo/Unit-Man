3b7472a301cfadcb93cef07192fcee4abf3ecf6d
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-06-01 16:21:49 -0300

    Create and add Inky subclass, has Enemy as base class

---
+++ Assets/Source/Inky.cs
@@ -0,0 +1,11 @@
+﻿namespace UnitMan.Source
+{
+    public class Inky : Enemy
+    {
+        protected override void Awake() {
+            base.Awake();
+            moveSpeed = 4f;
+            pathfindingIntervalSeconds = 3f;
+        }
+    }
+}
