50c297b8c70cba61ef3a81853efb4544f90edf92
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-06-08 16:31:52 -0300

    Create GameOverController class

---
+++ Assets/GameOverController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnitMan.Source.Utilities.TimeTracking;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnitMan
+{
+    public class GameOverController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private readonly Timer _returnTimer = new Timer(autoStart: true, isOneShot: true);
+
+        private void Awake() {
+            _returnTimer.OnEnd += ReturnToMainScreen;
+        }
+
+        private static void ReturnToMainScreen() {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+}
