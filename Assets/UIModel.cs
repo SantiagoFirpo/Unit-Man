d9f00f5e8ed5e89c0772d0f77e4fd76a9a5f8be1
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-06-09 14:28:29 -0300

    Update Invincible Timer

--- Assets/UIModel.cs
+++ Assets/UIModel.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnitMan.Source;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UnitMan
 {
@@ -21,22 +23,35 @@ namespace UnitMan
 
         public void LoseLife() {
             _lives--;
-            UpdateUI();
+            UpdateLivesLabel();
         }
 
-        private void UpdateUI() {
+        public void UpdateInvincibleTime(float newValue) {
+            float normalizedInvincibleTimer = 1f - newValue/PlayerController.INVINCIBLE_TIME_SECONDS;
+            UpdateInvincibleUI(normalizedInvincibleTimer);
+        }
+
+        private void UpdateLivesLabel() {
             _livesLabel.text = $"Lives: {_lives}";
             // _scoreLabel.text = $"Score: {_score}";
+        }
+
+        private void UpdateInvincibleUI(float newValue) {
+            _invincibleTimer.fillAmount = newValue;
         }
 
         // Start is called before the first frame update
         private int _lives = 3;
         private int _score;
+        private float _normalizedInvincibleTimer = 0f;
         
         [SerializeField]
         private TMP_Text _livesLabel;
         
         [SerializeField]
         private TMP_Text _scoreLabel;
+        
+        [SerializeField]
+        private Image _invincibleTimer;
     }
 }
