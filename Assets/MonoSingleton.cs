4f95ab91934ccaf888aa54b2d5eb0499607adeb8
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-06-11 15:45:59 -0300

    Import better sound effects

---
+++ Assets/MonoSingleton.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace UnitMan
+{
+    public class MonoSingleton : MonoBehaviour
+    {
+        
+    }
+}
