e7ec224e9baa9685e0258660dbf1ce0e0433bbe9
SantiagoFirpo <83927781+SantiagoFirpo@users.noreply.github.com>
2021-06-11 15:46:31 -0300

    Create AudioManager singleton for sound effects

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace UnitMan
+{
+    class AudioManager : MonoSingleton
+    {
+        public static AudioManager Instance {get; private set; }
+        
+        [FormerlySerializedAs("_track1")] [SerializeField]
+        private AudioSource _track0;
+
+        [SerializeField] private AudioClip _munch;
+        [SerializeField] private AudioClip _eatGhost;
+        [SerializeField] private AudioClip _death;
+        [SerializeField] private AudioClip _intermission;
+
+
+        public enum AudioEffectType
+        {
+            Munch, EatGhost, Death, Intermission
+        }
+
+        protected virtual void Awake() {
+            if (Instance != null) {
+                Destroy(gameObject);
+            }
+            else {
+                Instance = this;
+            }
+            
+        }
+
+        public void PlayClip(AudioEffectType effectType, int trackNumber) {
+            _track0.clip = effectType switch {
+                AudioEffectType.Munch => _munch,
+                AudioEffectType.EatGhost => _eatGhost,
+                AudioEffectType.Death => _death,
+                AudioEffectType.Intermission => _intermission,
+                _ => _track0.clip
+            };
+            _track0.Play();
+        }
+    }
+}
