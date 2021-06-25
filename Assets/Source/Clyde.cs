288fd16413d5e383fd1e1ca5f32821ab368c4428
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-06-25 15:40:45 -0300

    Implemented Clyde-specific movement

--- Assets/Source/Clyde.cs
+++ Assets/Source/Clyde.cs
@@ -1,4 +1,6 @@
-﻿namespace UnitMan.Source
+﻿using UnitMan.Source.Utilities.Pathfinding;
+
+namespace UnitMan.Source
 {
     public class Clyde : Enemy
     {
@@ -7,6 +9,15 @@
             pathfindingIntervalSeconds = 2f;
             base.Initialize();
             
+        }
+        
+        protected override void PollPlayerPosition()
+        {
+            currentTargetPosition =
+                PathGrid.TaxiCabDistance(gridPosition, playerController.gridPosition) > 8
+                    ? playerController.gridPosition //is further than 8 tiles away from pacman, target him
+                    : PathGrid.VectorToVector2Int(_bottomLeftMapBound); //is closer than 8 tiles away from pacman,
+                                                                        //go to bottom left corner
         }
     }
 }
