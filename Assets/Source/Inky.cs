b7f372014386b72f14f1ac6043c16f36bd942a0e
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-06-25 15:40:05 -0300

    Implemented Inky-specific movement

--- Assets/Source/Inky.cs
+++ Assets/Source/Inky.cs
@@ -1,12 +1,22 @@
-﻿namespace UnitMan.Source
+﻿using UnityEngine;
+
+namespace UnitMan.Source
 {
     public class Inky : Enemy
     {
+        [SerializeField]
+        private Enemy blinkyController;
         public override void Initialize() {
-            standardMoveSpeed = 4f;
+            standardMoveSpeed = 3.5f;
             pathfindingIntervalSeconds = 3f;
             base.Initialize();
             
+        }
+        
+        protected override void PollPlayerPosition()
+        {
+            currentTargetPosition = blinkyController.gridPosition +
+                                    (playerController.gridPosition - blinkyController.gridPosition) * 2;
         }
     }
 }
