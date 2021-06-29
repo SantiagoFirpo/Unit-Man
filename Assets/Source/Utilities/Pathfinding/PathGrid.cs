af64138900c28104c31d93f8e362b97f751b02f1
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-06-29 17:43:36 -0300

    Removed unused code and added Vector2.zero cache

--- Assets/Source/Utilities/Pathfinding/PathGrid.cs
+++ Assets/Source/Utilities/Pathfinding/PathGrid.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Collections.Generic;
+using UnitMan.Source.Entities.Actors;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -24,10 +24,12 @@ namespace UnitMan.Source.Utilities.Pathfinding
 
         private const int Y_SIZE = 26;
         private const int X_SIZE = 23;
-        public static Vector2Int UpVector2Int = Vector2Int.up;
-        public static Vector2Int DownVector2Int = Vector2Int.down;
-        public static Vector2Int LeftVector2Int = Vector2Int.left;
-        public static Vector2Int RightVector2Int = Vector2Int.right;
+
+        public static Vector2 zero = Vector2.zero;
+        public static Vector2Int upVector2Int = Vector2Int.up;
+        public static Vector2Int downVector2Int = Vector2Int.down;
+        public static Vector2Int leftVector2Int = Vector2Int.left;
+        public static Vector2Int rightVector2Int = Vector2Int.right;
         
         public static readonly Vector2 UpVector2 = Vector2.up;
         public static readonly Vector2 DownVector2 = Vector2.down;
@@ -36,7 +38,6 @@ namespace UnitMan.Source.Utilities.Pathfinding
         
 
         public Tilemap walkableTilemap;
-        public Tilemap wallTilemap;
 
         private readonly bool[][] _newGrid = new bool[Y_SIZE][]
         {
@@ -84,49 +85,9 @@ namespace UnitMan.Source.Utilities.Pathfinding
         public static int TaxiCabDistance(Vector2Int start, Vector2Int end) {
             return Mathf.Abs(start.y - end.y) + Mathf.Abs(start.x - end.x);
         }
-
-        public void CheckPossibleTurns(Vector3 position, bool[] turns, float colliderSize = 0.9f) {
-            Vector2Int tilePosition = VectorToVector2Int(position);
-            Vector2Int otherTilePosition = tilePosition;
-            float offsetX = position.x - tilePosition.x;
-            float offsetY = position.y - tilePosition.y;
-            
-            bool isBetweenHorizontal = Mathf.Abs(offsetY) < 0.1f && Mathf.Abs(offsetX) >= 0.1f;
-            bool isBetweenVertical = Mathf.Abs(offsetX) < 0.1f && Mathf.Abs(offsetY) >= 0.1f;
-            if (isBetweenHorizontal) {
-                bool isRight = offsetX > 0f;
-                if (isRight) {
-                    otherTilePosition = tilePosition + RightVector2Int;
-                }
-                else {
-                    otherTilePosition = tilePosition + LeftVector2Int;
-                }
-            }
-            else if (isBetweenVertical) {
-                bool isUp = offsetY > 0f;
-                if (isUp) {
-                    otherTilePosition = tilePosition + UpVector2Int;
-                }
-                else {
-                    otherTilePosition = tilePosition + DownVector2Int;
-                }
-            }
-            
-            bool canTurnUp = GetGridPosition(tilePosition.x, tilePosition.y + 1);
-            bool canTurnDown = GetGridPosition(tilePosition.x, tilePosition.y - 1);
-            bool canTurnLeft = GetGridPosition(tilePosition.x - 1, tilePosition.y);
-            bool canTurnRight = GetGridPosition(tilePosition.x + 1, tilePosition.y);
-            if (otherTilePosition != tilePosition) {
-                canTurnUp = canTurnUp && GetGridPosition(otherTilePosition.x, otherTilePosition.y + 1);
-                canTurnDown =  canTurnDown && GetGridPosition(otherTilePosition.x, otherTilePosition.y - 1);
-                canTurnLeft = canTurnLeft && GetGridPosition(otherTilePosition.x - 1, otherTilePosition.y);
-                canTurnRight = canTurnRight && GetGridPosition(otherTilePosition.x - 1, otherTilePosition.y);
-            }
-            turns[(int) Actor.Direction.Up] = canTurnUp;
-            turns[(int) Actor.Direction.Down] = canTurnDown;
-            turns[(int) Actor.Direction.Left] = canTurnLeft;
-            turns[(int) Actor.Direction.Right] = canTurnRight;
-            
+        
+        public static float TaxiCabDistanceVector3(Vector3 start, Vector3 end) {
+            return Mathf.Abs(start.y - end.y) + Mathf.Abs(start.x - end.x);
         }
 
         public void CheckPossibleTurns(Vector2Int position, bool[] turns) {
