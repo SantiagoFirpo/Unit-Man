159f665d92338a970c236c6b4889fc3a17520906
Santiago Alt <santiagofirpo4@gmail.com>
2021-06-29 14:55:00 -0300

    Reformatted and applied class name changes

--- Assets/Source/PlayerController.cs
+++ Assets/Source/PlayerController.cs
@@ -15,7 +15,7 @@ namespace UnitMan.Source
     {
         //TODO: screen wrap
         //TODO: death animation
-        
+
         private const float MOVE_SPEED = 5f;
         private InputMaps _inputMaps;
 
@@ -24,46 +24,50 @@ namespace UnitMan.Source
 
         public bool isInvincible;
         public readonly Timer invincibleTimer = new Timer(INVINCIBLE_TIME_SECONDS);
-        private SpriteRenderer _spriteRenderer;
         public static event Action<bool> OnInvincibleChanged;
         public const float INVINCIBLE_TIME_SECONDS = 10f;
-
-
-        // Start is called before the first frame update
-        protected override void Awake() {
-            base.Awake();
+        
+        public override void Initialize()
+        {
+            base.Initialize();
             _inputMaps = new InputMaps();
             _inputMaps.Player.Enable();
             _inputMaps.Player.Move.performed += OnMove;
             _playerInput = GetComponent<PlayerInput>();
-            _spriteRenderer = GetComponent<SpriteRenderer>();
-            // _playerInput.onActionTriggered += OnMove;
 
             invincibleTimer.OnEnd += DisableInvincibility;
-            currentDirection = PathGrid.Right;
+            currentDirection = PathGrid.RightVector2Int;
+            currentMoveSpeed = MOVE_SPEED;
         }
 
-        protected override void Update() {
+        protected override void Update()
+        {
             base.Update();
-            if (isInvincible) {
-                UIModel.Instance.UpdateInvincibleTime(invincibleTimer.currentTime);
+            if (isInvincible)
+            {
+                SessionDataModel.Instance.UpdateInvincibleTime(invincibleTimer.currentTime);
             }
         }
+        
 
-
-        private void OnDisable() {
+        protected override void UnsubscribeFromEvents()
+        {
+            base.UnsubscribeFromEvents();
             _playerInput.onActionTriggered -= OnMove;
-            _inputMaps.Player.Disable();
             invincibleTimer.OnEnd -= DisableInvincibility;
+            
+            _inputMaps.Player.Disable();
         }
 
-        private void DisableInvincibility() {
+        private void DisableInvincibility()
+        {
             isInvincible = false;
             OnInvincibleChanged?.Invoke(isInvincible);
-            AudioManager.Instance.PlayClip(AudioManager.AudioEffectType.Siren, 1, true);
+            AudioManagerSingle.Instance.PlayClip(AudioManagerSingle.AudioEffectType.Siren, 1, true);
         }
 
-        protected override void FixedUpdate() {
+        protected override void FixedUpdate()
+        {
             base.FixedUpdate();
             if (IsCardinalDirection(_inputVector))
             {
@@ -79,17 +83,17 @@ namespace UnitMan.Source
             UpdateMotion(new Vector2(currentDirection.x, currentDirection.y) * currentMoveSpeed);
         }
         
-
-        private void OnMove(InputAction.CallbackContext context) {
+        private void OnMove(InputAction.CallbackContext context)
+        {
             _inputVector = RoundToInt(context.ReadValue<Vector2>());
         }
 
-        public void SetInvincible() {
+        public void SetInvincible()
+        {
             isInvincible = true;
             invincibleTimer.Start();
-            AudioManager.Instance.PlayClip(AudioManager.AudioEffectType.PowerPellet, 1, true);
+            AudioManagerSingle.Instance.PlayClip(AudioManagerSingle.AudioEffectType.PowerPellet, 1, true);
             OnInvincibleChanged?.Invoke(isInvincible);
         }
-
     }
 }
