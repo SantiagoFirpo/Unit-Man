1ea28d21360fb3e5375038060b73f8a1388ecdc1
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-07-06 16:12:51 -0300

    Added method to calculate map bound positions just from origin global position and map dimensions

--- Assets/Source/Utilities/MazeData.cs
+++ Assets/Source/Utilities/MazeData.cs
@@ -6,9 +6,13 @@ namespace UnitMan.Source.Utilities
     public class MazeData : ScriptableObject
     {
         public Vector2Int hubPosition;
+        [HideInInspector]
         public Vector2Int topLeftMapPosition;
+        [HideInInspector]
         public Vector2Int topRightMapPosition;
+        [HideInInspector]
         public Vector2Int bottomLeftMapPosition;
+        [HideInInspector]
         public Vector2Int bottomRightMapPosition;
 
         public Vector2Int hubExitMarker;
@@ -19,5 +23,25 @@ namespace UnitMan.Source.Utilities
         [HideInInspector] public Vector2Int mapCentralPosition;
 
         public Vector2Int originPositionGlobal;
+
+        public int pelletCount;
+
+        public void CalculateBounds()
+        {
+            //origin position = -11, 4
+            //map dimensions = 23, 26
+            topLeftMapPosition = originPositionGlobal + new Vector2Int(-1, 1); // -12, 5
+            
+            topRightMapPosition =
+                new Vector2Int(originPositionGlobal.x + mapDimensions.x, originPositionGlobal.y + 1); // = 12, 5
+
+            bottomLeftMapPosition =
+                new Vector2Int(originPositionGlobal.x - 1, originPositionGlobal.y - mapDimensions.y); //-12, -22
+
+            bottomRightMapPosition = new Vector2Int(originPositionGlobal.x + mapDimensions.x,
+                                                    originPositionGlobal.y - mapDimensions.y); //12, -22
+
+            mapCentralPosition = originPositionGlobal + mapDimensions / 2;
+        }
     }
 }
