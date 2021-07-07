d63cf8ce44c250c7d53ec551a2a6348a4b3e314a
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-07-07 17:16:33 -0300

    Implemented Generic Observer System in TimerManager and moved it to Time Tracking

---
+++ Assets/Source/Utilities/TimeTracking/TimerManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnitMan.Source.Utilities.ObserverSystem;
+using UnityEngine;
+
+namespace UnitMan.Source.Utilities.TimeTracking {
+    public class TimerManager : MonoBehaviour, IEmitter<float>
+    {
+        public static TimerManager Instance { get; private set; }
+        private void Update() => EmitNotification(_listeningObservers, Time.deltaTime);
+
+        private readonly List<ObserverSystem.IObserver<float>> _listeningObservers =
+                    new List<ObserverSystem.IObserver<float>>(); 
+        
+        public void Attach(ObserverSystem.IObserver<float> observer) 
+        {
+            _listeningObservers.Add(observer);
+        }
+
+        public void EmitNotification(IEnumerable<ObserverSystem.IObserver<float>> observers, float deltaTime)
+        {
+            foreach (ObserverSystem.IObserver<float> observer in observers)
+            {
+                observer.OnNotified(this, Time.deltaTime);
+            }
+        }
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+            }
+            Instance = this;
+        }
+
+        public void Detach(ObserverSystem.IObserver<float> observer)
+        {
+            _listeningObservers.Remove(observer);
+        }
+    }
+}
