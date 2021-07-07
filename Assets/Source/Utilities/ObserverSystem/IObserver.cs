a87a707a67e8d2ce0606ff954bf10f47ee261ee7
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-07-07 16:20:20 -0300

    Added Observer Interface

---
+++ Assets/Source/Utilities/ObserverSystem/IObserver.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitMan.Source.Utilities.ObserverSystem
+{
+	public interface IObserver<TDataContainer>
+	{
+		public void OnObserved(IEmitter<TDataContainer> source, TDataContainer dataContainer);
+	}
+	
+	public interface IObserver
+	{
+		public void OnObserved(IEmitter source);
+	}
+}
