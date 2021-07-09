77de80fd439bd51084a5589719cdfaae1b1c3f6e
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-07-09 17:06:59 -0300

    Turned IObserver and IEmitter into regular classes

---
+++ Assets/Source/Utilities/ObserverSystem/Emitter.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+namespace UnitMan.Source.Utilities.ObserverSystem
+{
+	public class Emitter
+	{
+		private readonly List<Observer> _observers;
+		
+		public Emitter()
+		{
+			_observers = new List<Observer>();
+		}
+		public void Attach(Observer observer)
+		{
+			_observers.Add(observer);
+		}
+
+		public void EmitNotification()
+		{
+			foreach (Observer observer in _observers)//_observers seems to be altered in this foreach, copying it with ToList() solves
+														//this but generates a lot of garbage
+														//happens in PelletController and TimerManager
+			{
+				observer.OnNotified(this);
+			}
+		}
+
+		public void Detach(Observer observer)
+		{
+			_observers.Remove(observer);
+		}
+		
+		~Emitter()
+		{
+			foreach (Observer observer in _observers)
+			{
+				Detach(observer);
+			}
+		}
+	}
+	
+	public class Emitter<TDataContainer>
+	{
+		private readonly List<Observer<TDataContainer>> _observers;
+		public Emitter()
+		{
+			_observers = new List<Observer<TDataContainer>>();
+		}
+
+		public void Attach(Observer<TDataContainer> observer)
+		{
+			_observers.Add(observer);
+		}
+		
+		public void EmitNotification(TDataContainer dataContainer)
+		{
+			foreach (Observer<TDataContainer> observer in _observers)
+			{
+				observer.OnNotified(this, dataContainer);
+			}
+		}
+
+		public void Detach(Observer<TDataContainer> observer)
+		{
+			_observers.Remove(observer);
+		}
+		
+		~Emitter()
+		{
+			foreach (Observer<TDataContainer> observer in _observers)
+			{
+				Detach(observer);
+			}
+		}
+	}
+}
---
+++ Assets/Source/Utilities/ObserverSystem/Observer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UnitMan.Source.Utilities.ObserverSystem
+{
+	public class Observer
+	{
+		private readonly Action _onNotified;
+		public Observer(Action onNotified)
+		{
+			_onNotified = onNotified;
+		}
+		public void OnNotified(Emitter source)
+		{
+			_onNotified.Invoke();
+		}
+	}
+	public class Observer<TDataContainer>
+	{
+		private readonly Action<Emitter<TDataContainer>, TDataContainer> _onNotified;
+
+		public Observer(Action<Emitter<TDataContainer>, TDataContainer> onNotified)
+		{
+			_onNotified = onNotified;
+		}
+
+		public void OnNotified(Emitter<TDataContainer> source, TDataContainer dataContainer)
+		{
+			_onNotified.Invoke(source, dataContainer);
+		}
+	}
+	
+}
