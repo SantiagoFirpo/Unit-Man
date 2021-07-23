dcfa350ea0a312db3bad9a6dc74c28c696df1c20
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-07-23 16:51:28 -0300

    Hooked score submit to SessionDataModel Instance

--- Assets/Source/Management/Firebase/Firestore Leaderboard/FirestoreWriter.cs
+++ Assets/Source/Management/Firebase/Firestore Leaderboard/FirestoreWriter.cs
@@ -11,22 +11,34 @@ namespace UnitMan.Source.Management.Firebase.Firestore_Leaderboard
     public class FirestoreWriter : MonoBehaviour
     {
         [SerializeField]
-        private TMP_InputField _nameField;
+        private TMP_InputField nameField;
 
         private LeaderData _leaderData;
         
         
 
-        private void Awake()
-        {
-            _leaderData = new LeaderData("hhhhh", "Firpy", 981, true);
-        }
+        // private void Awake()
+        // {
+        //     _leaderData = new LeaderData(FirebaseAuthManager.Instance.auth.CurrentUser.UserId, "Firpy", 0, false);
+        // }
 
         public void SubmitScore()
         {
+            FirebaseUser currentUser = FirebaseAuthManager.Instance.auth.CurrentUser;
+            _leaderData = new LeaderData(currentUser.UserId,
+                                        nameField.text,
+                                        SessionDataModel.Instance.score,
+                                        SessionDataModel.Instance.won);
             Debug.Log("Should write to db");
-            FirebaseFirestore firestore = Firebase.Firestore.FirebaseFirestore.DefaultInstance;
+            FirebaseFirestore firestore = FirebaseFirestore.DefaultInstance;
             firestore.Document($"leaders/{_leaderData.PlayerID}").SetAsync(_leaderData);
+            GoToScoreboard();
+            
+        }
+
+        public void GoToScoreboard()
+        {
+            SceneManager.LoadScene("Scenes/Scoreboard");
         }
     }
 }
