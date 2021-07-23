70153f03f5af6f6d25386a762ca7ad2afeb83bac
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-07-23 16:49:10 -0300

    Added scoreboard formatting and sorting by score

--- Assets/Source/Management/Firebase/Firestore Leaderboard/FirestoreListener.cs
+++ Assets/Source/Management/Firebase/Firestore Leaderboard/FirestoreListener.cs
@@ -7,28 +7,53 @@ using UnityEngine.SceneManagement;
 
 namespace UnitMan.Source.Management.Firebase.Firestore_Leaderboard
 {
-    public class FirestoreListener : MonoBehaviour
-    {
-	    private IEnumerable<LeaderData> _localLeaders;
-        // Start is called before the first frame update
-        private void Start()
-        {
-	        FirebaseFirestore firestore = FirebaseFirestore.DefaultInstance;
-	        firestore.Collection("leaders").Listen(ListenCallback);
-        }
+	public class FirestoreListener : MonoBehaviour
+	{
+		[SerializeField] private TMP_Text scoreboardText;
 
-        private void ListenCallback(QuerySnapshot dataSnapshot)
-        {
-	        
-	        _localLeaders = dataSnapshot.Documents.Select(DocumentToLeaderData);
-	        foreach (LeaderData leader in _localLeaders)
-	        {
-		        Debug.Log(leader.PlayerDisplayName);
-		        Debug.Log(leader.Score);
-		        Debug.Log(leader.PlayerWon);
-	        }
-        }
+		private string _scoreboardTextBuffer;
 
-        private static LeaderData DocumentToLeaderData(DocumentSnapshot snapshot) => snapshot.ConvertTo<LeaderData>();
-    }
+		private IEnumerable<LeaderData> _localLeaders;
+
+		// Start is called before the first frame update
+		private void Start()
+		{
+			FirebaseFirestore firestore = FirebaseFirestore.DefaultInstance;
+			firestore.Collection("leaders").Listen(ListenCallback);
+			_scoreboardTextBuffer = "";
+		}
+
+		private void ListenCallback(QuerySnapshot dataSnapshot)
+		{
+			_scoreboardTextBuffer = "";
+			_localLeaders = dataSnapshot.Documents.Select(DocumentToLeaderData).OrderByDescending(ScoreSorter);
+			//TODO: add win based sorting
+			foreach (LeaderData leader in _localLeaders)
+			{
+				Debug.Log(leader.PlayerDisplayName);
+				Debug.Log(leader.Score);
+				Debug.Log(leader.PlayerWon);
+				string leaderWon = leader.PlayerWon ? "Yes" : "No";
+				_scoreboardTextBuffer =
+					$"{_scoreboardTextBuffer}Name: {leader.PlayerDisplayName} \n Score: {leader.Score} \n Player won? {leaderWon} \n \n";
+			}
+
+			scoreboardText.text = _scoreboardTextBuffer;
+
+
+		}
+
+		private static int ScoreSorter(LeaderData leader)
+		{
+			return leader.Score;
+		}
+
+		private static LeaderData DocumentToLeaderData(DocumentSnapshot snapshot) =>
+			snapshot.ConvertTo<LeaderData>();
+		
+		public void GoToMainMenu()
+		{
+			SceneManager.LoadScene("Scenes/Main Menu");
+		}
 	}
+}
