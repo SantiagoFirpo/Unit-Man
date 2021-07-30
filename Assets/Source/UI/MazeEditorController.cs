91ff2e9a9be5b65e3c034e94d828f6b37384ff8f
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-07-30 16:44:32 -0300

    Added working wall autotile at runtime

--- Assets/Source/UI/MazeEditorController.cs
+++ Assets/Source/UI/MazeEditorController.cs
@@ -1,5 +1,6 @@
 using System;
 using UnitMan.Source.Config;
+using UnitMan.Source.Utilities.Pathfinding;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
@@ -8,31 +9,39 @@ namespace UnitMan.Source.UI
 {
     public class MazeEditorController : MonoBehaviour
     {
+        private Maze _currentWorkingMaze;
         private InputMaps _inputMap;
         private Vector2 _mousePosition;
 
         [SerializeField]
-        private Tilemap wallTilemap;
+        private Tilemap _wallTilemap;
+
+        [SerializeField]
+        private TileBase wallRuleTile;
         
         private Vector3Int _wallOrigin;
+        private Camera _mainCamera;
+        private bool _isClicking = false;
 
         private void Awake()
         {
             _inputMap = new InputMaps();
             _inputMap.Enable();
             _inputMap.UI.Point.performed += OnMouseMove;
-            _inputMap.UI.Click.performed += OnClick;
+            _inputMap.UI.Click.performed += OnClickUpdated;
         }
 
         private void Start()
         {
-            _wallOrigin = wallTilemap.origin;
+            _wallOrigin = _wallTilemap.origin;
+            _mainCamera = Camera.main;
+            // wallRuleTile = _wallTilemap.GetTile(Vector3Int.zero);
         }
 
         private void OnDisable()
         {
             _inputMap.UI.Point.performed -= OnMouseMove;
-            _inputMap.UI.Click.performed -= OnClick;
+            _inputMap.UI.Click.performed -= OnClickUpdated;
         }
 
         private void OnMouseMove(InputAction.CallbackContext context)
@@ -40,9 +49,21 @@ namespace UnitMan.Source.UI
             _mousePosition = context.ReadValue<Vector2>();
         }
 
-        private void OnClick(InputAction.CallbackContext context)
+        private void Update()
         {
-            
+            Vector3Int mousePositionOnWallTileset = LevelGridController.Vector2ToVector3Int
+                                                                        (_mainCamera.ScreenToWorldPoint
+                                                                        (_mousePosition))  - _wallOrigin;
+            if (!_isClicking || _wallTilemap.GetTile(mousePositionOnWallTileset) == wallRuleTile) return;
+            _wallTilemap.SetTile(mousePositionOnWallTileset, wallRuleTile);
+        }
+
+        private void OnClickUpdated(InputAction.CallbackContext context)
+        {
+            _isClicking = !_isClicking;
         }
     }
+    
+    
+    
 }
