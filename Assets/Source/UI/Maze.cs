98c57bc9db7db6895c32ddfbb797308882b77e93
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-07-30 16:44:55 -0300

    Started Serializable Maze class

---
+++ Assets/Source/UI/Maze.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnitMan.Source.Config;
+using UnityEngine;
+
+namespace UnitMan.Source.UI
+{
+    [Serializable]
+    public class Maze
+    {
+        private MazeData _mazeData;
+        private Vector2Int[] _wallTiles;
+        private Vector2Int[] _pelletPositions;
+        private Vector2Int[] _powerPelletPositions;
+        private Vector2Int _playerStartingPosition;
+        private Vector2Int _ghostHouse;
+        private Dictionary<Vector2Int, GhostType> _ghostInitialPositions;
+    }
+
+    public enum GhostType
+    {
+        Blinky, Pinky, Inky, Clyde
+    }
+}
