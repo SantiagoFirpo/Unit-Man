e368bf01ac411f982d372d057a3321ed23598851
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-07-30 15:40:02 -0300

    Added mouse position and click callbacks

--- Assets/Source/UI/MazeEditorController.cs
+++ Assets/Source/UI/MazeEditorController.cs
@@ -8,18 +8,35 @@ namespace UnitMan.Source.UI
     public class MazeEditorController : MonoBehaviour
     {
         private InputMaps _inputMap;
+        private Vector2 _mousePosition;
+
+        [SerializeField]
+        private Tilemap wallTilemap;
+        
+        private Vector3Int _wallOrigin;
 
         private void Awake()
         {
             _inputMap = new InputMaps();
             _inputMap.Enable();
+            _inputMap.UI.Point.performed += OnMouseMove;
             _inputMap.UI.Click.performed += OnClick;
+        }
+
+        private void OnDisable()
+        {
+            _inputMap.UI.Point.performed -= OnMouseMove;
+            _inputMap.UI.Click.performed -= OnClick;
+        }
+
+        private void OnMouseMove(InputAction.CallbackContext context)
+        {
+            _mousePosition = context.ReadValue<Vector2>();
         }
 
         private void OnClick(InputAction.CallbackContext context)
         {
-            Debug.Log("hi");
-            Debug.Log(context.ReadValue<Vector2>());
+            
         }
     }
 }
