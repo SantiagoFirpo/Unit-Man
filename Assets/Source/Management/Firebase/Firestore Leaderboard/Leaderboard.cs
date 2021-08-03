18bfda0f8bfc8e237f553c791cde79f6f6f87603
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-08-03 16:02:57 -0300

    Created leaderboard wrapper class to contain all leaders and convert FirestoreLeaderData to LocalLeaderData

---
+++ Assets/Source/Management/Firebase/Firestore Leaderboard/Leaderboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnitMan.Source.UI;
+
+namespace UnitMan.Source.Management.Firebase.Firestore_Leaderboard
+{
+    [Serializable]
+    public class Leaderboard
+    {
+        public List<LocalLeaderData> values;
+
+        public Leaderboard(FirestoreLeaderData[] firestoreLeaders)
+        {
+            int firestoreLeadersLength = firestoreLeaders.Length;
+            values = new List<LocalLeaderData>(firestoreLeadersLength);
+            foreach (FirestoreLeaderData firestoreLeader in firestoreLeaders)
+            {
+                values.Add(new LocalLeaderData(firestoreLeader.PlayerDisplayName,
+                    firestoreLeader.Score,
+                    firestoreLeader.PlayerWon));
+            }
+            {
+                
+            }
+        }
+    }
+}
