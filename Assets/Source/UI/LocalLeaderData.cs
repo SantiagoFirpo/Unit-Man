df412ffa0895667290b6874d0fa97b5a0e8c7ce3
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-08-03 16:01:47 -0300

    Created LocalLeaderData to contain leader data as fields and to separate it from Firestore data

---
+++ Assets/Source/UI/LocalLeaderData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UnitMan.Source.UI
+{
+    [Serializable]
+    public class LocalLeaderData
+    {
+        public string playerDisplayName;
+        public int score;
+        public bool playerWon;
+
+        public LocalLeaderData(string playerDisplayName, int score, bool playerWon)
+        {
+            this.playerDisplayName = playerDisplayName;
+            this.score = score;
+            this.playerWon = playerWon;
+        }
+    }
+}
