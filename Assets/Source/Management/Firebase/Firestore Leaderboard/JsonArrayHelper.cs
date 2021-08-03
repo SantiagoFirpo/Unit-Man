6b0ad7b070cd30a6b83d81e7f6c43911ced963bb
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-08-03 16:03:53 -0300

    Imported JsonArrayHelper utility class

---
+++ Assets/Source/Management/Firebase/Firestore Leaderboard/JsonArrayHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnitMan.Source.Management.Firebase.Firestore_Leaderboard
+{
+    public static class JsonArrayHelper
+    {
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.items;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T> {items = array};
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        public static string ToJson<T>(T[] array, bool prettyPrint)
+        {
+            Wrapper<T> wrapper = new Wrapper<T> {items = array};
+            return JsonUtility.ToJson(wrapper, prettyPrint);
+        }
+
+        [Serializable]
+        private class Wrapper<T>
+        {
+            public T[] items;
+        }
+    }
+}
