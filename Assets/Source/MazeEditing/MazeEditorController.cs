1bb216bea0c9b92f4a409480315652c60f726f2c
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-08-06 17:28:30 -0300

    WIP: level object marking

--- Assets/Source/MazeEditing/MazeEditorController.cs
+++ Assets/Source/MazeEditing/MazeEditorController.cs
@@ -1,5 +1,3 @@
-using System;
-using UnitMan.Source.UI;
 using UnitMan.Source.Utilities.Pathfinding;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -27,7 +25,7 @@ namespace UnitMan.Source.MazeEditing
         private Camera _mainCamera;
         private bool _isLeftClicking;
         private bool _isRightClicking;
-        
+        private Vector2Int _mouseWorldPositionV2Int;
         [SerializeField]
         private GameObject pelletMarkerPrefab;
         
@@ -51,10 +49,10 @@ namespace UnitMan.Source.MazeEditing
             _inputMap = new Gameplay();
             _inputMap.Enable();
             _inputMap.UI.Point.performed += OnMouseMove;
-            _inputMap.UI.Click.performed += OnClickUpdated;
-            _inputMap.UI.RightClick.performed += OnRightClickUpdated;
-            _inputMap.UI.Click.performed += OnLeftClick;
-
+            _inputMap.UI.Click.started += OnLeftClicked;
+            _inputMap.UI.Click.canceled += OnLeftUnclicked;
+            _inputMap.UI.RightClick.started += OnRightClicked;
+            _inputMap.UI.RightClick.canceled += OnRightUnclicked;
             _currentWorkingMaze = new Maze();
 
         }
@@ -63,15 +61,16 @@ namespace UnitMan.Source.MazeEditing
         {
             _wallOrigin = wallTilemap.origin;
             _mainCamera = Camera.main;
-            // wallRuleTile = _wallTilemap.GetTile(Vector3Int.zero);
         }
 
         private void OnDisable()
         {
             _inputMap.UI.Point.performed -= OnMouseMove;
-            _inputMap.UI.Click.performed -= OnClickUpdated;
-            _inputMap.UI.RightClick.performed -= OnRightClickUpdated;
-            _inputMap.UI.Click.started -= OnLeftClick;
+            _inputMap.UI.Click.started -= OnLeftClicked;
+            _inputMap.UI.Click.canceled -= OnLeftUnclicked;
+            _inputMap.UI.RightClick.started -= OnRightClicked;
+            _inputMap.UI.RightClick.canceled -= OnRightUnclicked;
+            _currentWorkingMaze = new Maze();
         }
 
         public void OnWallButtonSelected()
@@ -108,49 +107,64 @@ namespace UnitMan.Source.MazeEditing
         {
             _selectedObjectType = MazeObjectType.PowerPellet;
         }
-
-        private void OnLeftClick(InputAction.CallbackContext context)
-        {
-            Debug.Log("Should place");
-            switch (_selectedObjectType)
-            {
-                case MazeObjectType.Wall:
-                    break;
-                case MazeObjectType.Pellet:
-                    PlaceLevelObject(pelletMarkerPrefab);
-                    break;
-                case MazeObjectType.PowerPellet:
-                    PlaceLevelObject(powerMarkerPrefab);
-                    break;
-                case MazeObjectType.Player:
-                    break;
-                case MazeObjectType.Blinky:
-                    PlaceLevelObject(blinkyMarkerPrefab);
-                    break;
-                case MazeObjectType.Pinky:
-                    PlaceLevelObject(pinkyMarkerPrefab);
-                    break;
-                case MazeObjectType.Inky:
-                    PlaceLevelObject(inkyMarkerPrefab);
-                    break;
-                case MazeObjectType.Clyde:
-                    PlaceLevelObject(clydeMarkerPrefab);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
+        //
+        // private void OnLeftClick(InputAction.CallbackContext context)
+        // {
+        //     if (!context.started) return;
+        //     Debug.Log("Should place");
+        //     Vector2Int mouseWorldPosition = LevelGridController.VectorToVector2Int(_mouseWorldPosition);
+        //     if (_currentWorkingMaze.levelObjects.ContainsKey(mouseWorldPosition)) return;
+        //     switch (_selectedObjectType)
+        //     {
+        //         case MazeObjectType.Wall:
+        //             _currentWorkingMaze.levelObjects.Add(mouseWorldPosition, MazeObjectType.Wall);
+        //             break;
+        //         case MazeObjectType.Pellet:
+        //             _currentWorkingMaze.levelObjects.Add(mouseWorldPosition, MazeObjectType.Pellet);
+        //             break;
+        //         case MazeObjectType.PowerPellet:
+        //             _currentWorkingMaze.levelObjects.Add(mouseWorldPosition, MazeObjectType.PowerPellet);
+        //             break;
+        //         case MazeObjectType.Player:
+        //             _currentWorkingMaze.playerPosition = mouseWorldPosition;
+        //             break;
+        //         case MazeObjectType.Blinky:
+        //             _currentWorkingMaze.levelObjects.Add(mouseWorldPosition, MazeObjectType.Blinky);
+        //
+        //             break;
+        //         case MazeObjectType.Pinky:
+        //             _currentWorkingMaze.levelObjects.Add(mouseWorldPosition, MazeObjectType.Pinky);
+        //
+        //             break;
+        //         case MazeObjectType.Inky:
+        //             _currentWorkingMaze.levelObjects.Add(mouseWorldPosition, MazeObjectType.Inky);
+        //
+        //             break;
+        //         case MazeObjectType.Clyde:
+        //             _currentWorkingMaze.levelObjects.Add(mouseWorldPosition, MazeObjectType.Clyde);
+        //
+        //             break;
+        //         default:
+        //             throw new ArgumentOutOfRangeException();
+        //     }
+        // }
 
         private void PlaceLevelObject(GameObject prefab)
         {
+            if (prefab is null) return;
             Instantiate(prefab, _mouseWorldPosition, Quaternion.identity);
         }
 
-        private void OnRightClickUpdated(InputAction.CallbackContext obj)
+        private void OnRightClicked(InputAction.CallbackContext context)
         {
-            _isRightClicking = !_isRightClicking;
+            _isRightClicking = true;
         }
-
+        
+        private void OnRightUnclicked(InputAction.CallbackContext context)
+        {
+            _isRightClicking = false;
+        }
+        
         private void OnMouseMove(InputAction.CallbackContext context)
         {
             _mouseScreenPosition = context.ReadValue<Vector2>();
@@ -162,31 +176,76 @@ namespace UnitMan.Source.MazeEditing
 
         private void Update()
         {
-            
             if (_isRightClicking)
             {
+                _mouseWorldPositionV2Int = LevelGridController.VectorToVector2Int(_mouseWorldPosition);
+                _currentWorkingMaze.levelObjects.Remove(_mouseWorldPositionV2Int);
                 wallTilemap.SetTile(_mouseTilesetPosition, null);
             }
 
-            else if (_isLeftClicking)
+            else if (_isLeftClicking && !_currentWorkingMaze.levelObjects.ContainsKey(_mouseWorldPositionV2Int))
                 switch (_selectedObjectType)
                 {
                     case MazeObjectType.Wall when wallTilemap.GetTile(_mouseTilesetPosition) != wallRuleTile:
+                    {
                         wallTilemap.SetTile(_mouseTilesetPosition, wallRuleTile);
                         break;
+                    }
                     case MazeObjectType.Pellet:
-                        //place pellet
+                    {
+                        _currentWorkingMaze.levelObjects.Add(_mouseWorldPositionV2Int, MazeObjectType.Pellet);
+                        PlaceLevelObject(pelletMarkerPrefab);
                         break;
+                    }
                     case MazeObjectType.PowerPellet:
+                    {
+                        _currentWorkingMaze.levelObjects.Add(_mouseWorldPositionV2Int, MazeObjectType.PowerPellet);
+                        PlaceLevelObject(powerMarkerPrefab);
                         break;
+                    }
                     case MazeObjectType.Player:
+                    {
+                        _currentWorkingMaze.playerPosition = _mouseWorldPositionV2Int;
+                        // PlaceLevelObject(playerMare);
                         break;
+                    }
+                    case MazeObjectType.Blinky:
+                    {
+                        PlaceLevelObject(blinkyMarkerPrefab);
+                        _currentWorkingMaze.levelObjects.Add(_mouseWorldPositionV2Int, MazeObjectType.Blinky);
+                    
+                        break;
+                    }
+                    case MazeObjectType.Pinky:
+                    {
+                        _currentWorkingMaze.levelObjects.Add(_mouseWorldPositionV2Int, MazeObjectType.Pinky);
+                        PlaceLevelObject(pinkyMarkerPrefab);
+                        break;
+                    }
+                    case MazeObjectType.Inky:
+                    {
+                        PlaceLevelObject(inkyMarkerPrefab);
+                        _currentWorkingMaze.levelObjects.Add(_mouseWorldPositionV2Int, MazeObjectType.Inky);
+                    
+                        break;
+                    }
+                    case MazeObjectType.Clyde:
+                    {
+                        PlaceLevelObject(clydeMarkerPrefab);
+                        _currentWorkingMaze.levelObjects.Add(_mouseWorldPositionV2Int, MazeObjectType.Clyde);
+                        break;
+                    }
                 }
         }
 
-        private void OnClickUpdated(InputAction.CallbackContext context)
+        private void OnLeftClicked(InputAction.CallbackContext context)
         {
-            _isLeftClicking = !_isLeftClicking;
+            _isLeftClicking = true;
+        }
+        
+        private void OnLeftUnclicked(InputAction.CallbackContext context)
+        {
+            _isLeftClicking = false;
         }
     }
 
