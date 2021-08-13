f733f076f4e388818751c86824b9d17e649e0f2d
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-08-13 16:02:00 -0300

    Added Utility methods, renamed all overloads and moved Vector-related methods to VectorUtil

--- Assets/Source/Utilities/Pathfinding/LevelGridController.cs
+++ Assets/Source/Utilities/Pathfinding/LevelGridController.cs
@@ -107,59 +107,6 @@ namespace UnitMan.Source.Utilities.Pathfinding
             
         }
 
-        public static bool VectorApproximately(Vector3 v1, Vector2Int v2, float toleranceInclusive) {
-            return (Mathf.Abs(v1.x - v2.x) <= toleranceInclusive && Mathf.Abs(v1.y - v2.y) <= toleranceInclusive);
-        }
-        public static bool VectorApproximately(Vector2 v1, Vector2Int v2, float toleranceInclusive) {
-            return (Mathf.Abs(v1.x - v2.x) <= toleranceInclusive && Mathf.Abs(v1.y - v2.y) <= toleranceInclusive);
-        }
-        public static bool VectorApproximately(Vector3 v1, Vector3 v2, float toleranceInclusive) {
-            return (Mathf.Abs(v1.x - v2.x) <= toleranceInclusive && Mathf.Abs(v1.y - v2.y) <= toleranceInclusive);
-        }
-        public static bool VectorApproximately(Vector2Int v1, Vector3 v2, float toleranceInclusive) {
-            return (Mathf.Abs(v1.x - v2.x) <= toleranceInclusive && Mathf.Abs(v1.y - v2.y) <= toleranceInclusive);
-        }
-
-        public static Vector2Int VectorToVector2Int(Vector3 vector) {
-            return new Vector2Int(Mathf.RoundToInt(vector.x),
-                                  Mathf.RoundToInt(vector.y));
-        }
-
-        public static Vector3 Vector2ToVector3(Vector2Int vector)
-        {
-            return new Vector3(vector.x, vector.y, 0f);
-        }
-        
-        public static Vector3 Vector2ToVector3(Vector2 vector)
-        {
-            return new Vector3(vector.x, vector.y, 0f);
-        }
-        
-        public static Vector3Int Vector2ToVector3Int(Vector2 vector)
-        {
-            return new Vector3Int(Mathf.RoundToInt(vector.x), Mathf.RoundToInt(vector.y), 0);
-        }
-        
-        public static Vector3 Vector2IntToVector3(Vector2Int vector)
-        {
-            return new Vector3(vector.x, vector.y, 0f);
-        }
-        
-        public static Vector3Int Vector3ToVector3Int(Vector3 vector)
-        {
-            return new Vector3Int(Mathf.RoundToInt(vector.x), Mathf.RoundToInt(vector.y), 0);
-        }
-        
-        public static Vector2Int Vector3IntToVector2Int(Vector3Int vector)
-        {
-            return new Vector2Int(vector.x, vector.y);
-        }
-
-        public static Vector3 Round(Vector3 vector)
-        {
-            return new Vector3(Mathf.Round(vector.x), Mathf.Round(vector.y), Mathf.Round(vector.z));
-        }
-
         private void OnDrawGizmos() {
             InitializeGrid();
             for (int x = -11; x <= 11; x++) {
