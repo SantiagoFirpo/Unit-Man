839a9cdf1346c05d55816e8593a7dce652520c36
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-08-24 14:57:17 -0300

    Defined IStateMachine

---
+++ Assets/Source/Entities/Actors/Ghosts/IStateMachine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UnitMan.Source.Entities.Actors.Ghosts
+{
+    public interface IStateMachine<T> where T : Enum
+    {
+        public T State { get; set; }
+        public T PreviousState { get; set; }
+        public void OnStateEntered();
+        public void SetState(T state);
+        public void OnStateExit();
+    }
+}
