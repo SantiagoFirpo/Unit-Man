6dace7164efe83513cc7621cd2f83b3f422c0ed4
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-08-24 15:00:32 -0300

    Experimented with Bindings and ViewModels

---
+++ Assets/Source/UI/IBindable.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+
+namespace UnitMan.Source.UI
+{
+    public interface IBindable<in T>
+    {
+        public void Update(T data);
+    }
+
+    public class LivesBinding : IBindable<int>
+    {
+        private readonly TMP_Text _text;
+
+        public LivesBinding(TMP_Text text)
+        {
+            _text = text;
+        }
+
+        public void Update(int data)
+        {
+            _text.SetText($"Lives: {data}");
+        }
+    }
+
+    public class PureBinding : IBindable<object>
+    {
+        private readonly TMP_Text _text;
+
+        public PureBinding(TMP_Text text)
+        {
+            _text = text;
+        }
+
+        public void Update(object data)
+        {
+            _text.SetText(data.ToString());
+        }
+    }
+}
