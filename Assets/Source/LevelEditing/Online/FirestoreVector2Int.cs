40639aae96a1e73b48a20f5887dbdfecad13d6ae
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-08-25 16:46:52 -0300

    Added Firestore-friendly vector struct

---
+++ Assets/Source/LevelEditing/Online/FirestoreVector2Int.cs
@@ -0,0 +1,25 @@
+﻿using Firebase.Firestore;
+using UnityEngine;
+
+namespace UnitMan.Source.LevelEditing.Online
+{
+    [FirestoreData]
+    public struct FirestoreVector2Int
+    {
+        [FirestoreProperty]
+        public int X { get; set; }
+        
+        [FirestoreProperty]
+        public int Y { get; set; }
+
+        public static FirestoreVector2Int FromVector2Int(Vector2Int vector2Int)
+        {
+            FirestoreVector2Int firestoreVector2Int = new FirestoreVector2Int
+            {
+                X = vector2Int.x,
+                Y = vector2Int.y
+            };
+            return firestoreVector2Int;
+        }
+    }
+}
