a4477b0cc267cdd99197f1099ebcf661b3d43adc
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-09-10 18:00:05 -0300

    Started drafting MVVC implementation

---
+++ Assets/Source/UI/MVVC/View.cs
@@ -0,0 +1,25 @@
+﻿using UnitMan.Source.Utilities.ObserverSystem;
+using UnityEngine;
+
+namespace UnitMan.Source.UI.MVVC
+{
+    public abstract class View<TState> : MonoBehaviour
+    {
+        public abstract void RenderView(TState props);
+        private ViewModel<TState> _viewModel;
+        private Observer<TState> _observer;
+        private Emitter<TState> _emitter;
+        
+        public void Initialize(ViewModel<TState> viewModel)
+        {
+            _viewModel = viewModel;
+            _observer = new Observer<TState>(OnViewModelStateChange);
+            _viewModel.emitter.Attach(_observer);
+        }
+
+        private void OnViewModelStateChange(Emitter<TState> source, TState newState)
+        {
+            RenderView(newState);
+        }
+    }
+}
---
+++ Assets/Source/UI/MVVC/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using UnitMan.Source.Utilities.ObserverSystem;
+
+namespace UnitMan.Source.UI.MVVC
+{
+    public abstract class ViewModel<TState>
+    {
+        private TState _state;
+        public readonly Emitter<TState> emitter = new Emitter<TState>();
+        public readonly Observer<TState> observer;
+
+        protected ViewModel()
+        {
+            this.observer = new Observer<TState>(OnStateChangeByUser);
+        }
+
+        protected abstract void OnStateChangeByUser(Emitter<TState> source, TState newState);
+
+        public void SetState(TState targetState)
+        {
+            this._state = targetState;
+            emitter.EmitNotification(targetState);
+        }
+    }
+}
