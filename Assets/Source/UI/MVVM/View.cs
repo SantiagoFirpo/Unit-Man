6f31675536ba3302d373a2e904ce775916061d9d
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-09-14 15:56:55 -0300

    Created View generic class

---
+++ Assets/Source/UI/MVVM/View.cs
@@ -0,0 +1,22 @@
+﻿using UnitMan.Source.Utilities.ObserverSystem;
+using UnityEngine;
+
+namespace UnitMan.Source.UI.MVVM
+{
+    public abstract class View<TState, TViewModel> : MonoBehaviour where TViewModel : ViewModel<TState>
+    {
+        private Observer<TState> _observer;
+        private readonly Emitter<TState> _emitter = new Emitter<TState>();
+        private TViewModel _viewModel;
+
+        protected abstract void Render(Emitter<TState> source, TState state);
+
+        public void Initialize(TViewModel viewModel)
+        {
+            _observer = new Observer<TState>(Render);
+            _viewModel = viewModel;
+            _viewModel.emitter.Attach(_observer);
+            _emitter.Attach(_viewModel.observer);
+        }
+    }
+}
