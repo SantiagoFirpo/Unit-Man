6d224729023fb902fa667a71f1f3dcb501a38645
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-09-14 15:51:01 -0300

    Created MVVMController to create components

---
+++ Assets/MVVMController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnitMan.Source.UI.MVVM;
+using UnityEngine;
+
+namespace UnitMan
+{
+    public class MVVMController : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Source/UI/MVVM/MVVMComponent.cs
@@ -0,0 +1,17 @@
+﻿namespace UnitMan.Source.UI.MVVM
+{
+    public class MVVMComponent<TState, TView, TViewModel, TModel> where TView : View<TState, TViewModel> where TModel : Model where TViewModel : ViewModel<TState>, new()
+    {
+        private TView _view;
+        private TViewModel _viewModel;
+        private TModel _model;
+
+        public MVVMComponent(TView view, TModel model)
+        {
+            _view = view;
+            _viewModel = new TViewModel();
+            _view.Initialize(_viewModel);
+            _model = model;
+        }
+    }
+}
