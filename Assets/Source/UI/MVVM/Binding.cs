d7d7d737d42ada43765da5f7a9fd79aa340d96ed
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-09-17 15:28:44 -0300

    Created Binding classes

---
+++ Assets/Source/UI/MVVM/Binding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UnitMan.Source.UI.MVVM
+{
+    [Serializable]
+    public class Binding<T> : UnityEvent<T>
+    {
+        [SerializeField]
+        private T _value;
+
+        public void SetValue(T value)
+        {
+            _value = value;
+            this.Invoke(value);
+        }
+
+        public T GetValue()
+        {
+            return _value;
+        }
+    }
+    [Serializable]
+    public class Binding : UnityEvent
+    {
+        public void Call()
+        {
+            this.Invoke();
+        }
+    }
+}
