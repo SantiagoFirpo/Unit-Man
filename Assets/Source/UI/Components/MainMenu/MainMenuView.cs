2354af0f81374c1df39c979934cfaf8a6cd56d71
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-09-17 16:54:07 -0300

    Added button and levelId field functionality to Main Menu VVM

--- Assets/Source/UI/Components/MainMenu/MainMenuView.cs
+++ Assets/Source/UI/Components/MainMenu/MainMenuView.cs
@@ -2,21 +2,12 @@
 using UnitMan.Source.Management.Firebase.Auth;
 using UnitMan.Source.UI.MVVM;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace UnitMan.Source.UI.Components.MainMenu
 {
     public class MainMenuView : View
     {
-        public Binding<string> emailBinding;
-        public Binding<string> passwordBinding;
-
-        [SerializeField]
-        private Binding loginBinding;
-        [SerializeField]
-        private Binding registerBinding;
-        [SerializeField]
-        private Binding signOutBinding;
+        [Header("UI Components")]
         [SerializeField]
         private TMP_InputField emailField;
 
@@ -24,8 +15,39 @@ namespace UnitMan.Source.UI.Components.MainMenu
         private TMP_InputField passwordField;
 
         [SerializeField]
+        private TMP_InputField levelIdField;
+        [Header("Auth Bindings")]
+        public OneWayBinding<string> emailBinding;
+        public OneWayBinding<string> passwordBinding;
+        
+
+        [SerializeField]
+        private OneWayBinding loginBinding;
+        [SerializeField]
+        private OneWayBinding registerBinding;
+        [SerializeField]
+        private OneWayBinding signOutBinding;
+
+        [SerializeField]
         private TMP_Text authMessage;
 
+        [Header("App Navigation Button Bindings")]
+
+        [SerializeField]
+        private OneWayBinding quitButtonBinding;
+
+        // [SerializeField]
+        // private Binding leaderboardButtonBinding;
+        
+        [SerializeField]
+        private OneWayBinding playButtonBinding;
+
+        [SerializeField]
+        private OneWayBinding levelEditorButtonBinding;
+
+        [Header("Custom Level Input Bindings")]
+        [SerializeField]
+        private OneWayBinding<string> levelIdBinding;
         public void OnEmailChanged(string email)
         {
             emailBinding.SetValue(email);
@@ -33,31 +55,25 @@ namespace UnitMan.Source.UI.Components.MainMenu
 
         public void OnAuthStatusChanged(FirebaseAuthManager.AuthStatus authStatus)
         {
-            authMessage.SetText(MainMenuController.AuthStatusToMessage(authStatus));
+            authMessage.SetText(MainMenuViewModel.AuthStatusToMessage(authStatus));
         }
         
-        public void OnPasswordChanged(string password)
-        {
-            passwordBinding.SetValue(password);
-        }
+        public void OnPasswordChanged(string password) => passwordBinding.SetValue(password);
 
-        public void OnLoginButtonPressed()
-        {
-            loginBinding.Call();
-        }
+        public void OnLoginButtonPressed() => loginBinding.Call();
 
-        public void OnRegisterButtonPressed()
-        {
-            registerBinding.Call();
-        }
+        public void OnRegisterButtonPressed() => registerBinding.Call();
 
-        public void OnSignOutButtonPressed()
-        {
-            signOutBinding.Call();
-        }
+        public void OnSignOutButtonPressed() => signOutBinding.Call();
 
-        protected override void Render()
-        {
-        }
+        public void OnLevelIdChanged() => levelIdBinding.SetValue(levelIdField.text);
+
+        public void OnLevelEditorButtonPressed() => levelEditorButtonBinding.Call();
+
+        public void OnQuitButtonPressed() => quitButtonBinding.Call();
+
+        public void OnPlayButtonPressed() => playButtonBinding.Call();
+
+        // public void OnLeaderboardButtonPressed() => leaderboardButtonBinding.Call();
     }
 }
--- Assets/Source/UI/Components/MainMenu/MainMenuViewModel.cs
+++ Assets/Source/UI/Components/MainMenu/MainMenuViewModel.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Firebase.Extensions;
+using Firebase.Firestore;
+using UnitMan.Source.LevelEditing;
+using UnitMan.Source.LevelEditing.Online;
 using UnitMan.Source.Management.Firebase.Auth;
+using UnitMan.Source.Management.Firebase.FirestoreLeaderboard;
+using UnitMan.Source.UI.Components.Text;
 using UnitMan.Source.UI.MVVM;
 using UnitMan.Source.Utilities.ObserverSystem;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace UnitMan.Source.UI.Components.MainMenu
 {
@@ -14,18 +22,27 @@ namespace UnitMan.Source.UI.Components.MainMenu
         private string password;
 
         [SerializeField]
-        private Binding<FirebaseAuthManager.AuthStatus> authStatusBinding;
+        private string levelId;
+
+        [SerializeField]
+        private OneWayBinding<string> authStatusMessageBinding;
 
         private Observer<FirebaseAuthManager.AuthStatus> _authObserver;
 
         private void Awake()
         {
+            #if UNITY_EDITOR
+                        Debug.unityLogger.logEnabled = true;
+            #else
+                                     Debug.unityLogger.logEnabled=false;
+            #endif
+                        Debug.Log("Main Menu should initialize");
             _authObserver = new Observer<FirebaseAuthManager.AuthStatus>(OnAuthChanged);
         }
 
         private void OnAuthChanged(FirebaseAuthManager.AuthStatus authStatus)
         {
-            authStatusBinding.SetValue(authStatus);
+            authStatusMessageBinding.SetValue(AuthStatusToMessage(authStatus));
         }
 
         public void OnEmailChanged(string newEmail)
@@ -38,25 +55,113 @@ namespace UnitMan.Source.UI.Components.MainMenu
             this.password = newPassword;
         }
 
-        public void Login()
+        public void OnLoginPressed()
         {
             FirebaseAuthManager.Instance.TryLoginUser(email, password);
         }
         
-        public void Register()
+        public void OnRegisterPressed()
         {
             FirebaseAuthManager.Instance.TryRegisterUser(email, password);
         }
 
-        public void SignOut()
+        public void OnSignOutPressed()
         {
             FirebaseAuthManager.Instance.SignOutUser();
         }
 
+        public void OnLevelIdChanged(string newLevelId)
+        {
+            levelId = newLevelId;
+        }
+
+        public void OnLevelEditorPressed()
+        {
+            SceneManager.LoadScene("Level Editor", LoadSceneMode.Single);
+        }
+
+        public void OnQuitPressed()
+        {
+            Application.Quit();
+        }
+
+        public void OnPlayButtonPressed()
+        {
+            try
+            {
+                LoadLocalLevel(levelId);
+                SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                Debug.Log("Failed to fetch locally, Downloading level from firestore");
+                //Display "loading..."
+                DownloadFirestoreLevelWithId(levelId);
+            }
+        }
+        
+        private static void LoadLocalLevel(string levelId)
+        {
+            CrossSceneLevelContainer.Instance.level = JsonUtility.FromJson<Level>(FirestoreListener.LoadStringFromJson(levelId));
+        }
+        
+        private static Level LevelJsonWrapper(FirestoreLevel firestoreLevel)
+        {
+            string levelJson = JsonUtility.ToJson(Level.FromFirestoreLevel(firestoreLevel));
+            Debug.Log(levelJson);
+            return JsonUtility.FromJson<Level>(levelJson);
+        }
+        private static void StoreLevelAndGoToGameplay(Task<DocumentSnapshot> task)
+        {
+            Level levelFromJson = LevelJsonWrapper(task.Result.ConvertTo<FirestoreLevel>());
+            CrossSceneLevelContainer.Instance.level = levelFromJson;
+            SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+        }
+        
+        private static void DownloadFirestoreLevelWithId(string levelId)
+        {
+            FirebaseFirestore.DefaultInstance.Document($"levels/{levelId}")
+                .GetSnapshotAsync()
+                .ContinueWithOnMainThread(task =>
+                {
+                    AggregateException aggregateException = task.Exception;
+                    if (aggregateException == null)
+                    {
+                        StoreLevelAndGoToGameplay(task);
+                    }
+                    else
+                    {
+                        Debug.LogException(aggregateException);
+                    }
+                });
+        }
 
         private void Start()
         {
             FirebaseAuthManager.Instance.authStateChangedEmitter.Attach(_authObserver);
+        }
+
+        public static string AuthStatusToMessage(FirebaseAuthManager.AuthStatus authStatus)
+        {
+            return authStatus switch
+            {
+                FirebaseAuthManager.AuthStatus.LoggingIn => "LOGGING IN",
+                FirebaseAuthManager.AuthStatus.Registering => "REGISTERING",
+                FirebaseAuthManager.AuthStatus.WaitingForUser => "PLEASE REGISTER/LOGIN BELOW:",
+                FirebaseAuthManager.AuthStatus.RegisterCanceled => "REGISTER WAS CANCELED",
+                FirebaseAuthManager.AuthStatus.RegisterError => "REGISTER ERROR",
+                FirebaseAuthManager.AuthStatus.RegisterSuccessful => $"REGISTER SUCCESSFUL! REGISTERED AS {FirebaseAuthManager.Instance.auth.CurrentUser.Email}",
+                FirebaseAuthManager.AuthStatus.LoginCanceled => "LOGIN WAS CANCELED",
+                FirebaseAuthManager.AuthStatus.LoginError => "LOGIN ERROR",
+                FirebaseAuthManager.AuthStatus.LoginSuccessful => $"LOGIN SUCCESSFUL! LOGGED IN AS {FirebaseAuthManager.Instance.auth.CurrentUser.Email}",
+                FirebaseAuthManager.AuthStatus.SignOut => "SIGNED OUT",
+                FirebaseAuthManager.AuthStatus.Empty => "",
+                FirebaseAuthManager.AuthStatus.RegisterRequested => "REGISTERING",
+                FirebaseAuthManager.AuthStatus.LoginRequested => "LOGGING IN",
+                FirebaseAuthManager.AuthStatus.SignOutRequested => "SIGNING OUT",
+                _ => throw new ArgumentOutOfRangeException(nameof(authStatus), authStatus, null)
+            };
         }
     }
 }
