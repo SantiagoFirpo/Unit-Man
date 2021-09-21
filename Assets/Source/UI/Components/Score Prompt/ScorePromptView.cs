3d3d9946f502cd8350b9ec139bb889fac73239e9
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-09-21 16:08:25 -0300

    Created ScorePromptView

---
+++ Assets/Source/UI/Components/Score Prompt/ScorePromptView.cs
@@ -0,0 +1,42 @@
+﻿using TMPro;
+using UnitMan.Source.UI.MVVM;
+using UnityEngine;
+
+namespace UnitMan.Source.UI.Components.Score_Prompt
+{
+    public class ScorePromptView : View
+
+    {
+        [SerializeField]
+        private TMP_Text scoreLabel;
+
+        [SerializeField]
+        private OneWayBinding<string> nameBinding = new OneWayBinding<string>();
+
+        [SerializeField]
+        private OneWayBinding yesButtonBinding = new OneWayBinding();
+
+        [SerializeField]
+        private OneWayBinding noButtonBinding = new OneWayBinding();
+
+        public void OnScoreChanged(int newScore)
+        {
+            scoreLabel.SetText($"SCORE: {newScore}");
+        }
+
+        public void OnNameChanged(string newName)
+        {
+            nameBinding.SetValue(newName);
+        }
+
+        public void OnYesPressed()
+        {
+            yesButtonBinding.Call();
+        }
+
+        public void OnNoPressed()
+        {
+            noButtonBinding.Call();
+        }
+    }
+}
