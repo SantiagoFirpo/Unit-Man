b3d11a88d57cb44f4d0fa3510f95b154251fd0b7
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-09-21 16:07:56 -0300

    Created ScorePromptViewModel

---
+++ Assets/Source/UI/Components/Score Prompt/ScorePromptViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnitMan.Source.Management.Firebase.FirestoreLeaderboard;
+using UnitMan.Source.Management.Session;
+using UnitMan.Source.UI.MVVM;
+using UnityEngine;
+
+namespace UnitMan.Source.UI.Components.Score_Prompt
+{
+    public class ScorePromptViewModel : ViewModel
+    {
+        [SerializeField]
+        private OneWayBinding<int> score;
+        private string _playerName;
+
+        [SerializeField]
+        private FirestoreWriter firestoreWriter;
+
+        public void OnNameChanged(string newName)
+        {
+            _playerName = newName;
+        }
+
+        public void OnNo()
+        {
+            FirestoreWriter.GoToScoreboard();
+        }
+
+        public void OnYes()
+        {
+            firestoreWriter.SubmitScore(_playerName);
+        }
+
+        private void Start()
+        {
+            score.SetValue(SessionViewModel.Instance.score);
+        }
+    }
+}
