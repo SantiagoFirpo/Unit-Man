cf040a290501f3a1713d3233fde223618b4289f8
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-09-22 17:43:18 -0300

    Organized Serialized fields

--- Assets/Source/UI/Components/LevelEditor/LevelEditorView.cs
+++ Assets/Source/UI/Components/LevelEditor/LevelEditorView.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using TMPro;
 using UnitMan.Source.UI.MVVM;
 using UnitMan.Source.Utilities.Pathfinding;
-using UnitMan.Source.Utilities.TimeTracking;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -13,86 +10,8 @@ namespace UnitMan.Source.UI.Components.LevelEditor
 {
     public class LevelEditorView : View
     {
-        [SerializeField]
-        private OneWayBinding<string> levelIdBinding;
+        [Header("Bindings")]
 
-        [SerializeField]
-        private Transform brushPreviewTransform;
-
-        [SerializeField]
-        private SpriteRenderer brushPreviewSprite;
-        
-        [SerializeField]
-        private GameObject pelletMarkerPrefab;
-        
-        [SerializeField]
-        private GameObject powerMarkerPrefab;
-        
-        [SerializeField]
-        private GameObject blinkyMarkerPrefab;
-        
-        [SerializeField]
-        private GameObject pinkyMarkerPrefab;
-        
-        [SerializeField]
-        private GameObject inkyMarkerPrefab;
-        
-        [SerializeField]
-        private GameObject clydeMarkerPrefab;
-        
-        private readonly Dictionary<Vector3, GameObject> _localObjects = new Dictionary<Vector3, GameObject>();
-        
-        [SerializeField]
-        private Transform pacManTransform;
-
-        [SerializeField]
-        private Transform ghostHouseTransform;
-        
-        private EventSystem _eventSystem;
-        
-        private SpriteRenderer _brushPreviewSprite;
-        [SerializeField]
-        private Sprite wallIcon;
-        [SerializeField]
-        private Sprite pelletIcon;
-        [SerializeField]
-        private Sprite powerPelletIcon;
-        [SerializeField]
-        private Sprite blinkyIcon;
-        [SerializeField]
-        private Sprite pinkyIcon;
-        [SerializeField]
-        private Sprite inkyIcon;
-        [SerializeField]
-        private Sprite clydeIcon;
-        [SerializeField]
-        private Sprite pacManIcon;
-        [SerializeField]
-        private Sprite houseIcon;
-
-        private Quaternion _identity;
-        [SerializeField]
-        private Transform ghostDoor;
-
-        [SerializeField]
-        private Sprite doorIcon;
-
-        [SerializeField]
-        private GameObject clipboardPingText;
-        
-        private Timer _clipboardPingTimer;
-        private Timer _uploadPingTimer;
-
-        
-        [SerializeField]
-        private GameObject uiCanvas;
-
-        [SerializeField]
-        private TMP_InputField levelIdField;
-
-        [SerializeField]
-        private GameObject uploadPingText;
-        
         [SerializeField]
         private OneWayBinding uploadBinding;
 
@@ -113,17 +32,13 @@ namespace UnitMan.Source.UI.Components.LevelEditor
 
         [SerializeField]
         private OneWayBinding<BrushType> brushTypeBinding;
-
-        private Camera _mainCamera;
-        private Vector3 _mouseScreenPosition;
-        [SerializeField]
-        private Tilemap wallTilemap;
         
-        private Gameplay _inputMap;
+        [SerializeField]
+        private OneWayBinding<string> levelIdBinding;
+        
         [SerializeField]
         private OneWayBinding<bool> leftClickBinding;
 
-        private bool _wasPointerOverUI;
         [SerializeField]
         private OneWayBinding<bool> pointerOverUIBinding;
 
@@ -131,19 +46,61 @@ namespace UnitMan.Source.UI.Components.LevelEditor
         private OneWayBinding<bool> rightClickBinding;
 
         [SerializeField]
-        private OneWayBinding<Vector3Int> _mouseTilesetPosition;
+        private OneWayBinding<Vector3Int> mouseTilesetPosition;
 
         [SerializeField]
-        private OneWayBinding<Vector3> _mouseWorldPosition;
+        private OneWayBinding<Vector3> mouseWorldPosition;
+        
+        [Header("Icons")]
 
+        [SerializeField]
+        private Sprite wallIcon;
+        [SerializeField]
+        private Sprite pelletIcon;
+        [SerializeField]
+        private Sprite powerPelletIcon;
+        [SerializeField]
+        private Sprite blinkyIcon;
+        [SerializeField]
+        private Sprite pinkyIcon;
+        [SerializeField]
+        private Sprite inkyIcon;
+        [SerializeField]
+        private Sprite clydeIcon;
+        [SerializeField]
+        private Sprite pacManIcon;
+        [SerializeField]
+        private Sprite houseIcon;
+
+        [SerializeField]
+        private Sprite doorIcon;
+        
+        [SerializeField]
+        private Transform brushPreviewTransform;
+
+        private EventSystem _eventSystem;
+        
+        [SerializeField]
+        private SpriteRenderer brushPreviewSprite;
+
+        [Header("Dependencies")]
+        
+        [SerializeField]
+        private GameObject uiCanvas;
+        private Camera _mainCamera;
+        private Vector3 _mouseScreenPosition;
+        
+        [SerializeField]
+        private Tilemap wallTilemap;
+        
+        private Gameplay _inputMap;
+
+        [SerializeField]
+        private ToastViewModel toast;
+
+        private bool _wasPointerOverUI;
         private void Start()
         {
-            _clipboardPingTimer = new Timer(3f, false, true);
-            _clipboardPingTimer.OnEnd += ClipboardPingTimerOnOnEnd;
-
-            _uploadPingTimer = new Timer(3f, false, true);
-            _uploadPingTimer.OnEnd += UploadPingTimerOnOnEnd;
-            
             _mainCamera = Camera.main;
             _eventSystem = EventSystem.current;
             brushPreviewSprite = brushPreviewTransform.GetComponent<SpriteRenderer>();
@@ -178,16 +135,6 @@ namespace UnitMan.Source.UI.Components.LevelEditor
             rightClickBinding.SetValue(false);
         }
 
-        private void UploadPingTimerOnOnEnd()
-        {
-            uploadPingText.SetActive(false);
-        }
-
-        private void ClipboardPingTimerOnOnEnd()
-        {
-            clipboardPingText.SetActive(false);
-        }
-
         public void OnLevelIdChanged(string newLevelId)
         {
             levelIdBinding.SetValue(newLevelId);
@@ -210,27 +157,20 @@ namespace UnitMan.Source.UI.Components.LevelEditor
                 _ => throw new ArgumentOutOfRangeException(nameof(brushType), brushType, null)
             };
         }
-        
-        private void PingUserClipboard()
-        {
-            clipboardPingText.SetActive(true);
-            _clipboardPingTimer.Start();
-        }
 
-        private void PingLevelUpload()
+        public void PingLevelUpload()
         {
-            uploadPingText.SetActive(true);
-            _uploadPingTimer.Start();
+            toast.Notify("LEVEL UPLOADED SUCCESSFULLY!");
         }
         
         private void OnMouseMove(InputAction.CallbackContext context)
         {
             _mouseScreenPosition = VectorUtil.ToVector3(context.ReadValue<Vector2>());
-            _mouseTilesetPosition.SetValue(wallTilemap.WorldToCell(_mainCamera.ScreenToWorldPoint(_mouseScreenPosition)));
+            mouseTilesetPosition.SetValue(wallTilemap.WorldToCell(_mainCamera.ScreenToWorldPoint(_mouseScreenPosition)));
 
             Vector3 mouseWorldPositionBuffer = VectorUtil.Round(_mainCamera.ScreenToWorldPoint(_mouseScreenPosition));
             mouseWorldPositionBuffer.z = 0f;
-            _mouseWorldPosition.SetValue(mouseWorldPositionBuffer);
+            mouseWorldPosition.SetValue(mouseWorldPositionBuffer);
             if (_eventSystem.IsPointerOverGameObject()) return;
             brushPreviewTransform.position = mouseWorldPositionBuffer;
         }
@@ -258,9 +198,15 @@ namespace UnitMan.Source.UI.Components.LevelEditor
 
         }
 
+        public void PingClipboard()
+        {
+            toast.Notify("LEVEL ID COPIED TO CLIPBOARD!");
+        }
+        
+
         public void OnBrushChanged(BrushType newValue)
         {
-            _brushPreviewSprite.sprite = BrushTypeToIcon(newValue);
+            brushPreviewSprite.sprite = BrushTypeToIcon(newValue);
         }
 
         public void ToggleUI(bool newValue)
