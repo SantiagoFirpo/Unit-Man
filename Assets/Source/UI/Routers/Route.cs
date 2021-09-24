02e4c853265ab18a8678c9473ba5a7a8daaeea23
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-09-24 15:51:32 -0300

    Added Route class

---
+++ Assets/Source/UI/Routers/Route.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnitMan.Source.UI.Routers
+{
+    [Serializable]
+    public class Route<TEnumType>
+    {
+        [SerializeField]
+        private GameObject view;
+        [SerializeField]
+        private TEnumType value;
+
+
+        public TEnumType GetValue() => value;
+        public Route(TEnumType value)
+        {
+            this.value = value;
+        }
+
+        public void Render()
+        {
+            view.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            view.SetActive(false);
+        }
+    }
+}
