4d736124614f71ae1b396f49143d6045f7503c95
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-09-24 17:36:45 -0300

    WIP: Level Cell for Local Level Explorer

---
+++ Assets/LevelCellView.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnitMan.Source.UI.MVVM;
+using UnityEngine;
+
+namespace UnitMan
+{
+    public class LevelCellView : View
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
