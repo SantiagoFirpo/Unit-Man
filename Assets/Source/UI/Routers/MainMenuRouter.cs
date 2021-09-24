bed9fac381557220678a36bbc0cdb3b2c2c6fdc7
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-09-24 16:06:23 -0300

    Implemented MainMenuRouter from Router

---
+++ Assets/Source/UI/Routers/MainMenuRouter.cs
@@ -0,0 +1,31 @@
+﻿using UnitMan.Source.UI.MVVM;
+
+namespace UnitMan.Source.UI.Routers
+{
+    public class MainMenuRouter : Router<MainMenuRouter.MainMenuRouteValue>
+    {
+        public static MainMenuRouter Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+
+        public enum MainMenuRouteValue
+        {
+            Undefined, Auth, Home
+        }
+
+        public override void OnStateEntered()
+        {
+            RenderWithValue(state);
+        }
+    }
+}
