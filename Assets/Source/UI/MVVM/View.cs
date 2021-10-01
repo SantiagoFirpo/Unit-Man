34aa5e8e981c16d3e905dd18ab02c0e6758a0e87
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-10-01 15:42:28 -0300

    Added util methods

--- Assets/Source/UI/MVVM/View.cs
+++ Assets/Source/UI/MVVM/View.cs
@@ -7,5 +7,16 @@ namespace UnitMan.Source.UI.MVVM
     [Serializable]
     public abstract class View : MonoBehaviour
     {
+        public GameObject GameObject {get; private set;}
+
+        private void Awake()
+        {
+            GameObject = gameObject;
+        }
+
+        public void Hide()
+        {
+            GameObject.SetActive(false);
+        }
     }
 }
