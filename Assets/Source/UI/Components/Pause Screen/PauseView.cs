f9e59520baa291409c6d31f4b119e19344a4c0ac
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-10-06 14:42:32 -0300

    Created Pause Screen component

---
+++ Assets/Source/UI/Components/Pause Screen/PauseView.cs
@@ -0,0 +1,19 @@
+using UnitMan.Source.UI.MVVM;
+
+namespace UnitMan.Source.UI.Components.Pause_Screen
+{
+    public class PauseView : View
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/Source/UI/Components/Pause Screen/PauseViewModel.cs
@@ -0,0 +1,12 @@
+using UnitMan.Source.UI.MVVM;
+
+namespace UnitMan.Source.UI.Components.Pause_Screen
+{
+    public class PauseViewModel : ViewModel
+    {
+
+        public override void OnRendered()
+        {
+        }
+    }
+}
