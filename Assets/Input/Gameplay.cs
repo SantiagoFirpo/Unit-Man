fcbf62809d04d7605351dc9fd8a93899e4256ee2
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-10-06 16:51:36 -0300

    Added Virtual Joystick to actions

--- Assets/Input/Gameplay.cs
+++ Assets/Input/Gameplay.cs
@@ -34,7 +34,7 @@ public class @Gameplay : IInputActionCollection, IDisposable
                     ""path"": ""<Gamepad>/leftStick"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": "";Gamepad"",
+                    ""groups"": ""Gamepad;Touch;Joystick"",
                     ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
