6ac66588eb0db3f20f62e4cb89b8b8d5b79522dc
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-10-13 16:26:16 -0300

    Added reroute to Online Level Explorer

--- Assets/Source/UI/Components/ConfirmLevelDeleteOnline/ConfirmLevelDeleteOnlineViewModel.cs
+++ Assets/Source/UI/Components/ConfirmLevelDeleteOnline/ConfirmLevelDeleteOnlineViewModel.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.IO;
-using Firebase.Firestore;
-using UnitMan.Source.Management.Firebase.Auth;
+﻿using Firebase.Firestore;
 using UnitMan.Source.UI.Components.ConfirmLevelDeleteDialogueBox;
-using UnitMan.Source.UI.Components.YesNoDialogueBox;
-using UnitMan.Source.UI.MVVM;
 using UnitMan.Source.UI.Routing.Routers;
 using UnitMan.Source.Utilities;
 using UnityEngine;
@@ -15,8 +10,10 @@ namespace UnitMan.Source.UI.Components.ConfirmLevelDeleteOnline
     {
         public override void YesPressed()
         {
+            Debug.Log("ConfirmLevelDeleteOnline");
             FirebaseFirestore.DefaultInstance.Document($"levels/{LevelIdToDeleteContainer.Instance.level.id}")
                 .DeleteAsync();
+            MainMenuRouter.Instance.SetState(MainMenuRouter.MainMenuRoute.OnlineLevelExplorer);
         }
 
         public override void NoPressed()
