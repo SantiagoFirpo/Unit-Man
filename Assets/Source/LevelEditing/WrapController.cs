1d866a38fa05d902dddcf83eb0510b7a00f5b833
Santiago Firpo <santiago.firpo@aquiris.com.br>
2021-10-15 16:39:22 -0300

    Created WrapController class

---
+++ Assets/Source/LevelEditing/WrapController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UnitMan.Source.LevelEditing
+{
+    public class WrapController : MonoBehaviour
+    {
+        private WrapController _destination;
+
+        public void SetDestination(WrapController newDestination)
+        {
+            if (_destination != null) return;
+            _destination = newDestination;
+        }
+
+        public void SyncWithDestination()
+        {
+            _destination._destination = this;
+        }
+    }
+}
